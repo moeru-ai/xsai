f6dca5025f674efcdb4c2ea6fd71e587d7933f71
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-23 23:00:59 +0800

    chore(xsai): add import

--- packages/xsai/src/index.ts
+++ packages/xsai/src/index.ts
@@ -1,3 +1,6 @@
+// @xsai/tool extend types
+import '@xsai/tool/generate-text'
+
 export { embed, embedMany } from '@xsai/embed'
 export { generateText } from '@xsai/generate-text'
 export { tool } from '@xsai/tool'
