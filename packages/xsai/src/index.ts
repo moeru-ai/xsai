aea9a40e2be412640909dadc3fa0ce66d5eb9747
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-24 20:37:09 +0800

    chore(xsai): update export

--- packages/xsai/src/index.ts
+++ packages/xsai/src/index.ts
@@ -3,4 +3,5 @@ import '@xsai/tool/generate-text'
 
 export { embed, embedMany } from '@xsai/embed'
 export { generateText } from '@xsai/generate-text'
+export { streamText } from '@xsai/stream-text'
 export { tool } from '@xsai/tool'
