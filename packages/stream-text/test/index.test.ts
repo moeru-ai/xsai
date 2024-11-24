70b90cf9b9051619c10a6893058ec836579e9ef1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-24 20:25:17 +0800

    chore(stream-text): update test

--- packages/stream-text/test/index.test.ts
+++ packages/stream-text/test/index.test.ts
@@ -26,4 +26,30 @@ describe('@xsai/stream-text', () => {
 
     expect(result.join('')).toStrictEqual('YES')
   })
+
+  it('42', async () => {
+    const { textStream } = await streamText({
+      messages: [
+        {
+          content: 'You are a helpful assistant.',
+          role: 'system',
+        },
+        {
+          content: 'What is the Answer to the Ultimate Question of Life, The Universe, and Everything?',
+          role: 'user',
+        },
+      ],
+      model: 'llama3.2',
+      seed: 42,
+    })
+
+    const result: string[] = []
+
+    for await (const textPart of textStream) {
+      result.push(textPart)
+    }
+
+    expect(result.join('')).toMatchSnapshot()
+    expect(result).toMatchSnapshot()
+  })
 })
