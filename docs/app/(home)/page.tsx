58673c889d0ecbc53d74b8fbafa5c7d508c8a2b1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-28 21:54:06 +0800

    docs: update hero section

--- docs/app/(home)/page.tsx
+++ docs/app/(home)/page.tsx
@@ -1,23 +1,33 @@
-import { AuroraBackground } from '@/components/ui/aurora-background'
-import Link from 'next/link'
+import { BackgroundLines } from '@/components/ui/background-lines'
+import { HoverBorderGradient } from '@/components/ui/hover-border-gradient'
+import { Spotlight } from '@/components/ui/spotlight'
+import Link from 'fumadocs-core/link'
 import React from 'react'
 
 export default () => (
-  <AuroraBackground className="-mt-14">
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="text-fd-foreground mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open
+  <BackgroundLines className="-mt-14 h-screen w-full rounded-md flex md:items-center md:justify-center relative overflow-hidden">
+    <Spotlight
+      // className="-top-56"
+      fill="white"
+    />
+    <div className="flex flex-col items-center gap-8 p-4 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0">
+      <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-fd-foreground to-fd-muted-foreground bg-opacity-50">
+        <small>extra-small</small>
         {' '}
-        <Link
-          className="text-fd-foreground font-semibold underline"
-          href="/docs"
-        >
-          /docs
-        </Link>
-        {' '}
-        and see the documentation.
-      </p>
-    </main>
-  </AuroraBackground>
+        <span>AI SDK</span>
+        <br />
+        <small>for any OpenAI-compatible API.</small>
+      </h1>
+      {/* <p className="mt-4 font-normal text-base text-muted-foreground max-w-lg text-center mx-auto">
+        lorem ipsum
+      </p> */}
+      <HoverBorderGradient
+        as="button"
+        className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
+        containerClassName="rounded-full"
+      >
+        <Link href="/docs">Get Started</Link>
+      </HoverBorderGradient>
+    </div>
+  </BackgroundLines>
 )
