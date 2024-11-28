1de14da9c2abd8eaa87ea414ec0993b204a54639
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-28 21:13:43 +0800

    feat(docs): add aceternity-ui

--- docs/app/(home)/page.tsx
+++ docs/app/(home)/page.tsx
@@ -1,19 +1,23 @@
+import { AuroraBackground } from '@/components/ui/aurora-background'
 import Link from 'next/link'
+import React from 'react'
 
 export default () => (
-  <main className="flex flex-1 flex-col justify-center text-center">
-    <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-    <p className="text-fd-muted-foreground">
-      You can open
-      {' '}
-      <Link
-        className="text-fd-foreground font-semibold underline"
-        href="/docs"
-      >
-        /docs
-      </Link>
-      {' '}
-      and see the documentation.
-    </p>
-  </main>
+  <AuroraBackground className="-mt-14">
+    <main className="flex flex-1 flex-col justify-center text-center">
+      <h1 className="text-fd-foreground mb-4 text-2xl font-bold">Hello World</h1>
+      <p className="text-fd-muted-foreground">
+        You can open
+        {' '}
+        <Link
+          className="text-fd-foreground font-semibold underline"
+          href="/docs"
+        >
+          /docs
+        </Link>
+        {' '}
+        and see the documentation.
+      </p>
+    </main>
+  </AuroraBackground>
 )
--- docs/tailwind.config.ts
+++ docs/tailwind.config.ts
@@ -1,16 +1,70 @@
 import type { Config } from 'tailwindcss'
 
 import { createPreset } from 'fumadocs-ui/tailwind-plugin'
+// @ts-expect-error missing types
+import flattenColorPalette from 'tailwindcss/lib/util/flattenColorPalette'
+import animate from 'tailwindcss-animate'
+
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme('colors'))
+  const newVars = Object.fromEntries(
+    Object.entries(allColors)
+      .map(([key, val]) => [`--${key}`, val]),
+  )
+
+  addBase({
+    ':root': newVars,
+  })
+}
 
 export default {
   content: [
-    './components/**/*.{ts,tsx}',
+    // './pages/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    // './src/**/*.{ts,tsx}',
     './content/**/*.{md,mdx}',
     './mdx-components.{ts,tsx}',
     './node_modules/fumadocs-ui/dist/**/*.js',
   ],
+  darkMode: ['class'],
+  plugins: [animate, addVariablesForColors],
+  // prefix: '',
   presets: [createPreset({
     preset: 'black',
   })],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+        'aurora': 'aurora 60s linear infinite',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+        'aurora': {
+          from: {
+            backgroundPosition: '50% 50%, 50% 50%',
+          },
+          to: {
+            backgroundPosition: '350% 50%, 350% 50%',
+          },
+        },
+      },
+    },
+  },
 } satisfies Config
