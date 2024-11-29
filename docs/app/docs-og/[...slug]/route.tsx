da061edb6d158ab03dbbada787094595eb57cb85
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-29 16:26:45 +0800

    chore(docs): update og-image

--- docs/app/docs-og/[...slug]/route.tsx
+++ docs/app/docs-og/[...slug]/route.tsx
@@ -3,7 +3,9 @@ import { generateOGImage } from 'fumadocs-ui/og'
 
 export const GET = metadataImage.createAPI(page => generateOGImage({
   description: page.data.description,
-  icon: (<img height="16" src="https://github.com/moeru-ai.png" width="16" />),
+  icon: (<img height="64" src="https://github.com/moeru-ai.png" width="64" />),
+  primaryColor: '#222',
+  primaryTextColor: '#fff',
   site: 'xsAI',
   title: page.data.title,
 }))
