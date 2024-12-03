ff6b5c10e10415a28ce011dfb7de974a34dd5753
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-03 19:49:51 +0800

    chore(shared): add comments

--- packages/shared/src/types/common-request-options.ts
+++ packages/shared/src/types/common-request-options.ts
@@ -2,6 +2,8 @@ export interface CommonRequestOptions {
   abortSignal?: AbortSignal
   /** @default `undefined` */
   headers?: Headers | Record<string, string>
+  /** @example `gpt-4o` */
   model: string
+  /** @example `https://openai.com/v1/chat/completions` */
   url: string | URL
 }
