9e1c7cea7f2d00322155341388a9e0c8d2214846
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-03 20:02:01 +0800

    fix(shared/clean): clean undefined only

--- packages/shared/src/utils/clean.ts
+++ packages/shared/src/utils/clean.ts
@@ -1,1 +1,1 @@
-export const clean = <T extends Record<string, undefined | unknown> = Record<string, undefined | unknown>>(record: T) => Object.fromEntries(Object.entries(record).filter(([, v]) => !!v)) as Record<keyof T, Exclude<T[keyof T], unknown>>
+export const clean = <T extends Record<string, undefined | unknown> = Record<string, undefined | unknown>>(record: T) => Object.fromEntries(Object.entries(record).filter(([, v]) => v === undefined)) as Record<keyof T, Exclude<T[keyof T], unknown>>
