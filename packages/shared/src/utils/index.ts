66b9c17e442bdee1363088a38422f9e012e9976c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-03 19:55:30 +0800

    refactor(shared): remove requestUrl

--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -1,3 +1,2 @@
 export { objCamelToSnake, strCamelToSnake } from './case'
 export { clean } from './clean'
-export { requestUrl } from './url'
--- packages/shared/src/utils/url.ts
+++
@@ -1,1 +0,0 @@
-export const requestUrl = (path: string, base: string | URL = 'http://localhost:11434/v1/') => new URL(path, base)
