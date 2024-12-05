6f53b3e8c08e9b22a1fa5dddf1945f7c75a9c704
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-05 16:08:58 +0800

    chore(bumpp): set recursive

--- bump.config.ts
+++ bump.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'bumpp'
-import { globSync as glob } from 'node:fs'
 
 export default defineConfig({
   all: true,
-  files: glob('**/package.json'),
   push: false,
+  recursive: true,
 })
