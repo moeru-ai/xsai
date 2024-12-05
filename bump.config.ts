331ddc3170e1d3ada398f08754b5174e54f6225f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-05 15:58:50 +0800

    chore: release v0.0.15

--- bump.config.ts
+++ bump.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'bumpp'
+import { globSync as glob } from 'node:fs'
 
 export default defineConfig({
   all: true,
+  files: glob('**/package.json'),
   push: false,
 })
