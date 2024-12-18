29056590576f2ad87f898af632574f6c52072878
Neko <neko@ayaka.moe>
2024-12-18 15:38:00 +0800

    chore(shared-chat): reuse types (#12)

--- packages/shared-chat/src/utils/message.ts
+++ packages/shared-chat/src/utils/message.ts
@@ -1,15 +1,15 @@
-import type { AssistantMessage, Message, SystemMessage, ToolCall, ToolMessage, UserMessage } from '../types/message'
-import type { AudioPart, ImagePart, TextPart } from '../types/message-part'
+import type { AssistantMessage, AssistantMessagePart, Message, SystemMessage, SystemMessagePart, ToolCall, ToolMessage, ToolMessagePart, UserMessage, UserMessagePart } from '../types/message'
+import type { ImagePart, TextPart } from '../types/message-part'
 
 export function messages(...messages: Message[]): Message[] {
   return messages
 }
 
-export function system(content: string): SystemMessage {
+export function system<C extends string | SystemMessagePart[]>(content: C): SystemMessage {
   return { content, role: 'system' }
 }
 
-export function user<C extends Array<AudioPart | ImagePart | TextPart> | string>(content: C): UserMessage {
+export function user<C extends Array<UserMessagePart> | string>(content: C): UserMessage {
   return { content, role: 'user' }
 }
 
@@ -21,14 +21,16 @@ export function imagePart(url: string): ImagePart {
   return { image_url: { url }, type: 'image_url' }
 }
 
-export function assistant<C extends string | ToolCall>(content: C): AssistantMessage {
+export function assistant<C extends AssistantMessagePart[] | string | ToolCall>(content: C): AssistantMessage {
   if (typeof content === 'string')
+    return { content, role: 'assistant' }
+  if (Array.isArray(content))
     return { content, role: 'assistant' }
 
   return { role: 'assistant', tool_calls: [content] }
 }
 
-export function tool(content: string, toolCall: ToolCall): ToolMessage {
+export function tool<C extends string | ToolMessagePart[]>(content: C, toolCall: ToolCall): ToolMessage {
   return {
     content,
     role: 'tool',
