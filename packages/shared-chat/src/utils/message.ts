7392f3a1f44771dac49f64d6c44805b718518696
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-18 20:46:16 +0800

    refactor(shared-chat/message): arrow function

--- packages/shared-chat/src/utils/message.ts
+++ packages/shared-chat/src/utils/message.ts
@@ -1,39 +1,23 @@
 import type { AssistantMessage, AssistantMessagePart, Message, SystemMessage, SystemMessagePart, ToolCall, ToolMessage, ToolMessagePart, UserMessage, UserMessagePart } from '../types/message'
 import type { ImagePart, TextPart } from '../types/message-part'
 
-export function messages(...messages: Message[]): Message[] {
-  return messages
-}
+export const messages = (...messages: Message[]): Message[] => messages
 
-export function system<C extends string | SystemMessagePart[]>(content: C): SystemMessage {
-  return { content, role: 'system' }
-}
+export const system = <C extends string | SystemMessagePart[]>(content: C): SystemMessage => ({ content, role: 'system' })
 
-export function user<C extends Array<UserMessagePart> | string>(content: C): UserMessage {
-  return { content, role: 'user' }
-}
+export const user = <C extends Array<UserMessagePart> | string>(content: C): UserMessage => ({ content, role: 'user' })
 
-export function textPart(text: string): TextPart {
-  return { text, type: 'text' }
-}
+export const textPart = (text: string): TextPart => ({ text, type: 'text' })
 
-export function imagePart(url: string): ImagePart {
-  return { image_url: { url }, type: 'image_url' }
-}
+export const imagePart = (url: string): ImagePart => ({ image_url: { url }, type: 'image_url' })
 
-export function assistant<C extends AssistantMessagePart[] | string | ToolCall>(content: C): AssistantMessage {
-  if (typeof content === 'string')
-    return { content, role: 'assistant' }
-  if (Array.isArray(content))
-    return { content, role: 'assistant' }
+export const assistant = <C extends AssistantMessagePart[] | string | ToolCall>(content: C): AssistantMessage =>
+  (typeof content === 'string' || Array.isArray(content))
+    ? { content, role: 'assistant' }
+    : { role: 'assistant', tool_calls: [content] }
 
-  return { role: 'assistant', tool_calls: [content] }
-}
-
-export function tool<C extends string | ToolMessagePart[]>(content: C, toolCall: ToolCall): ToolMessage {
-  return {
-    content,
-    role: 'tool',
-    tool_call_id: toolCall.id,
-  }
-}
+export const tool = <C extends string | ToolMessagePart[]>(content: C, toolCall: ToolCall): ToolMessage => ({
+  content,
+  role: 'tool',
+  tool_call_id: toolCall.id,
+})
