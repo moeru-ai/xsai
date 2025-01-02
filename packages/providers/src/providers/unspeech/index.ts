1a928d6577a3b12b499200a53aa08feb454c2aba
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-01-02 14:46:51 +0800

    feat(providers): add unspeech

---
+++ packages/providers/src/providers/unspeech/index.ts
@@ -0,0 +1,22 @@
+import type { ProviderOptions, SpeechProvider } from '../../types'
+
+import { generateCRO } from '../../utils/generate-cro'
+
+export interface UnSpeechOptions {
+  /** @experimental */
+  extraBody?: Record<string, unknown>
+  model: `elevenlabs/${string}` | `koemotion/${string}` | `openai/${string}` | ({} & string)
+  voice: string
+}
+
+export const createUnSpeech = (userOptions?: ProviderOptions<false>): SpeechProvider => ({
+  speech: (options: UnSpeechOptions) => ({
+    ...options,
+    ...generateCRO(options.model, {
+      ...userOptions,
+      baseURL: userOptions?.baseURL ?? new URL('http://localhost:5933/v1/'),
+    }),
+  }),
+})
+
+export const unSpeech = createUnSpeech()
