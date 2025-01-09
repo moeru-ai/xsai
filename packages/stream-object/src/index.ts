c9914c7a356be286d7ad35ac7be3622bb7d713c9
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-01-09 20:08:42 +0800

    fix(stream-object): fix interface name

--- packages/stream-object/src/index.ts
+++ packages/stream-object/src/index.ts
@@ -5,18 +5,18 @@ import { clean } from '@xsai/shared'
 import { streamText, type StreamTextOptions, type StreamTextResult } from '@xsai/stream-text'
 import { parse } from 'best-effort-json-parser'
 
-export interface GenerateObjectOptions<T extends Schema> extends StreamTextOptions {
+export interface StreamObjectOptions<T extends Schema> extends StreamTextOptions {
   schema: T
   schemaDescription?: string
   schemaName?: string
 }
 
-export interface GenerateObjectResult<T extends Schema> extends StreamTextResult {
+export interface StreamObjectResult<T extends Schema> extends StreamTextResult {
   partialObjectStream: ReadableStream<PartialDeep<Infer<T>>>
 }
 
 /** @experimental WIP */
-export const streamObject = async <T extends Schema>(options: GenerateObjectOptions<T>): Promise<GenerateObjectResult<T>> =>
+export const streamObject = async <T extends Schema>(options: StreamObjectOptions<T>): Promise<StreamObjectResult<T>> =>
   await streamText({
     ...options,
     response_format: {
