67903b795fa525d4720ed613629727d4afe19e73
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-01-10 14:29:17 +0800

    chore(providers/google-generative-ai): update model list

--- packages/providers/src/providers/google-generative-ai.ts
+++ packages/providers/src/providers/google-generative-ai.ts
@@ -3,8 +3,17 @@ import type { ChatProvider, EmbedProvider, ModelProvider, ProviderOptions, Provi
 import { generateCRO } from '../utils/generate-cro'
 
 export const createGoogleGenerativeAI = (userOptions: ProviderOptions<true>):
-  /** @see {@link https://ai.google.dev/gemini-api/docs/models/gemini} */
-  ChatProvider<'gemini-1.5-flash' | 'gemini-1.5-flash-8b' | 'gemini-1.5-pro' | 'gemini-2.0-flash-exp'>
+  /** @see {@link https://aistudio.google.com/u/1/prompts/new_chat} */
+  ChatProvider<
+    // gemini 1.5
+    'gemini-1.5-flash' | 'gemini-1.5-flash-8b' | 'gemini-1.5-pro' |
+    // preview
+    'gemini-2.0-flash-exp' | 'gemini-2.0-flash-thinking-exp-1219' | 'gemini-exp-1206' |
+    // gemma
+    'gemma-2-2b-it' | 'gemma-2-9b-it' | 'gemma-2-27b-it' |
+    // preview
+    'lernlm-1.5-pro-experimental'
+  >
   & EmbedProvider<'text-embedding-004'>
   & ModelProvider => {
   const options: ProviderResult = {
