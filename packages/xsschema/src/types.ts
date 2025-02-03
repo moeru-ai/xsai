3b1a313d629dc594f45f0dc3fb25dfc4201b476d
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-03 22:35:07 +0800

    chore(xsschema): export schema type

--- packages/xsschema/src/types.ts
+++ packages/xsschema/src/types.ts
@@ -1,5 +1,7 @@
 import type { StandardSchemaV1 } from '@standard-schema/spec'
 
+export type { StandardSchemaV1 as Schema } from '@standard-schema/spec'
+
 export type Infer<T extends StandardSchemaV1> = StandardSchemaV1.InferOutput<T>
 
 export type InferIn<T extends StandardSchemaV1> = StandardSchemaV1.InferInput<T>
