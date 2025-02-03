eaf6993447f4b67c875f16b629e2f2867274fa62
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-03 23:20:54 +0800

    fix(tool): drop tool result generic type

--- packages/tool/src/generate-text.ts
+++ packages/tool/src/generate-text.ts
@@ -1,11 +1,9 @@
 /// <reference types="@xsai/generate-text" />
 
-import type { Schema } from 'xsschema'
-
 import type { ToolResult } from '.'
 
 declare module '@xsai/generate-text' {
   export interface GenerateTextOptions {
-    tools?: ToolResult<Schema>[]
+    tools?: ToolResult[]
   }
 }
--- packages/tool/src/index.ts
+++ packages/tool/src/index.ts
@@ -3,7 +3,7 @@ import type { InferIn, Schema } from 'xsschema'
 import { clean } from '@xsai/shared'
 import { toJSONSchema } from 'xsschema'
 
-export interface ToolOptions<T extends Schema> {
+export interface ToolOptions<T extends Schema = Schema> {
   description?: string
   execute: (input: InferIn<T>) => Promise<string> | string
   name: string
@@ -11,8 +11,8 @@ export interface ToolOptions<T extends Schema> {
   strict?: boolean
 }
 
-export interface ToolResult<T extends Schema> {
-  execute: (input: InferIn<T>) => Promise<string> | string
+export interface ToolResult {
+  execute: (input: unknown) => Promise<string> | string
   function: {
     description?: string
     name: string
@@ -22,7 +22,7 @@ export interface ToolResult<T extends Schema> {
   type: 'function'
 }
 
-export const tool = async <T extends Schema>(options: ToolOptions<T>): Promise<ToolResult<T>> => ({
+export const tool = async <T extends Schema>(options: ToolOptions<T>): Promise<ToolResult> => ({
   execute: options.execute,
   function: {
     description: options.description,
