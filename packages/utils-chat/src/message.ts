a3736371f74230363e278340d4efdfffaa50374b
Neko <neko@ayaka.moe>
2025-02-03 10:13:48 +0800

    fix(utils-chat): array tool calls incorrectly constructed (#39)
    Co-authored-by: LemonNeko <chheese048@gmail.com>

--- packages/utils-chat/src/message.ts
+++ packages/utils-chat/src/message.ts
@@ -10,10 +10,29 @@ export const textPart = (text: string): TextPart => ({ text, type: 'text' })
 
 export const imagePart = (url: string): ImagePart => ({ image_url: { url }, type: 'image_url' })
 
-export const assistant = <C extends AssistantMessagePart[] | string | ToolCall>(content: C): AssistantMessage =>
-  (typeof content === 'string' || Array.isArray(content))
-    ? { content, role: 'assistant' }
-    : { role: 'assistant', tool_calls: [content] }
+export const isToolCall = (content: AssistantMessagePart[] | string | ToolCall | ToolCall[]): content is ToolCall | ToolCall[] => {
+  const isElementToolCallLike = (c: AssistantMessagePart | AssistantMessagePart[] | string | ToolCall | ToolCall[]) => {
+    return (typeof c === 'object'
+      && (
+        ('type' in c && c.type === 'function')
+        && 'id' in c
+        && 'function' in c && typeof c.function === 'object')
+    )
+  }
+
+  return isElementToolCallLike(content)
+    || (Array.isArray(content) && content.every(part => isElementToolCallLike(part)))
+}
+
+export const assistant = <C extends AssistantMessagePart[] | string | ToolCall | ToolCall[]>(content: C): AssistantMessage => {
+  if (isToolCall(content)) {
+    return Array.isArray(content)
+      ? { role: 'assistant', tool_calls: content }
+      : { role: 'assistant', tool_calls: [content] }
+  }
+
+  return { content, role: 'assistant' }
+}
 
 export const tool = <C extends string | ToolMessagePart[]>(content: C, toolCall: ToolCall): ToolMessage => ({
   content,
