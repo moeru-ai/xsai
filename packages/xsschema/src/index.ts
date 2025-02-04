5bc15fe0aea98e5ae0c8ee2c9dde3ba097f73bad
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-04 13:24:04 +0800

    feat(xsschema): export json schema type

--- packages/xsschema/src/index.ts
+++ packages/xsschema/src/index.ts
@@ -1,3 +1,3 @@
 export { toJsonSchema, toJsonSchema as toJSONSchema } from './to-json-schema'
-export type { Infer, InferIn, Schema } from './types'
+export type { Infer, InferIn, JSONSchema as JsonSchema, JSONSchema, Schema } from './types'
 export { validate } from './validate'
