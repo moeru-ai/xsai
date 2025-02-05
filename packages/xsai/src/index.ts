635282e0a7eb113fc25c884e4d603e5978075225
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-05 21:06:33 +0800

    chore(xsai): update exports

--- packages/xsai/src/index.ts
+++ packages/xsai/src/index.ts
@@ -3,14 +3,17 @@ import '@xsai/tool/generate-text'
 
 // @xsai/providers
 export * as providers from './providers'
-export { embed, embedMany, type EmbedManyOptions, type EmbedManyResult, type EmbedOptions, type EmbedResponseUsage, type EmbedResult } from '@xsai/embed'
-export { generateObject, type GenerateObjectOptions, type GenerateObjectResult } from '@xsai/generate-object'
-export { generateSpeech, type GenerateSpeechOptions } from '@xsai/generate-speech'
-export { generateText, type GenerateTextOptions, type GenerateTextResponse, type GenerateTextResult, type StepResult } from '@xsai/generate-text'
-export { generateTranscription, type GenerateTranscriptionOptions, type GenerateTranscriptionResult } from '@xsai/generate-transcription'
-export { listModels, type ListModelsOptions, type ListModelsResponse, type Model, retrieveModel, type RetrieveModelOptions } from '@xsai/model'
-// shared chat utils
+
+// xsai core utils
+export * from '@xsai/embed'
+export * from '@xsai/generate-object'
+export * from '@xsai/generate-speech'
+export * from '@xsai/generate-text'
+export * from '@xsai/generate-transcription'
+export * from '@xsai/model'
 export * from '@xsai/shared-chat'
-export { streamObject, type StreamObjectOptions, type StreamObjectResult } from '@xsai/stream-object'
-export { type ChunkResult, streamText, type StreamTextOptions, type StreamTextResult } from '@xsai/stream-text'
-export { tool, type ToolOptions, type ToolResult } from '@xsai/tool'
+export * from '@xsai/stream-object'
+export * from '@xsai/stream-text'
+export * from '@xsai/tool'
+export * from '@xsai/utils-chat'
+export * from '@xsai/utils-stream'
