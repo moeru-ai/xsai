349ee11aaaf9e20563f2f2bf477e9caea8b6e7eb
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-17 00:12:32 +0800

    chore(generate-text): move tool calling text

--- packages/generate-text/test/index.test.ts
+++ packages/generate-text/test/index.test.ts
@@ -1,4 +1,6 @@
 import { ollama } from '@xsai/providers'
+import { tool } from '@xsai/tool'
+import { description, object, pipe, string } from 'valibot'
 import { describe, expect, it } from 'vitest'
 
 import type { GenerateTextStepResult } from '../src'
@@ -34,4 +36,50 @@ describe('@xsai/generate-text', () => {
 
     expect(steps[0]).toStrictEqual(step)
   })
+
+  it('with tool calling', async () => {
+    const weather = await tool({
+      description: 'Get the weather in a location',
+      execute: ({ location }) => JSON.stringify({
+        location,
+        temperature: 42,
+      }),
+      name: 'weather',
+      parameters: object({
+        location: pipe(
+          string(),
+          description('The location to get the weather for'),
+        ),
+      }),
+    })
+
+    const { steps, text } = await generateText({
+      ...ollama.chat('mistral-nemo'),
+      maxSteps: 2,
+      messages: [
+        {
+          content: 'You are a helpful assistant.',
+          role: 'system',
+        },
+        {
+          content: 'What is the weather in San Francisco? do not answer anything else.',
+          role: 'user',
+        },
+      ],
+      seed: 42,
+      toolChoice: 'required',
+      tools: [weather],
+    })
+
+    expect(text).toMatchSnapshot()
+
+    const { toolCalls, toolResults } = steps[0]
+
+    expect(toolCalls[0].toolName).toBe('weather')
+    expect(toolCalls[0].args).toBe('{"location":"San Francisco"}')
+
+    expect(toolCalls[0].toolName).toBe('weather')
+    expect(toolResults[0].args).toStrictEqual({ location: 'San Francisco' })
+    expect(toolResults[0].result).toBe('{"location":"San Francisco","temperature":42}')
+  }, 20000)
 })
--- packages/tool/test/index.test.ts
+++ packages/tool/test/index.test.ts
@@ -1,19 +1,20 @@
-import { generateText } from '@xsai/generate-text'
-import { ollama } from '@xsai/providers'
 import { description, object, pipe, string } from 'valibot'
 import { describe, expect, it } from 'vitest'
 
 import { tool } from '../src'
 
 describe('@xsai/tool', () => {
-  it('generateText with tool', async () => {
+  it('weather', async () => {
+    const name = 'weather'
+    const desc = 'Get the weather in a location'
+
     const weather = await tool({
-      description: 'Get the weather in a location',
+      description: desc,
       execute: ({ location }) => JSON.stringify({
         location,
         temperature: 42,
       }),
-      name: 'weather',
+      name,
       parameters: object({
         location: pipe(
           string(),
@@ -22,33 +23,20 @@ describe('@xsai/tool', () => {
       }),
     })
 
-    const { steps, text } = await generateText({
-      ...ollama.chat('mistral-nemo'),
-      maxSteps: 2,
-      messages: [
-        {
-          content: 'You are a helpful assistant.',
-          role: 'system',
+    expect(weather.type).toBe('function')
+    expect(weather.function.name).toBe(name)
+    expect(weather.function.description).toBe(desc)
+    expect(weather.function.parameters).toStrictEqual({
+      properties: {
+        location: {
+          description: 'The location to get the weather for',
+          type: 'string',
         },
-        {
-          content: 'What is the weather in San Francisco? do not answer anything else.',
-          role: 'user',
-        },
+      },
+      required: [
+        'location',
       ],
-      seed: 42,
-      toolChoice: 'required',
-      tools: [weather],
+      type: 'object',
     })
-
-    expect(text).toMatchSnapshot()
-
-    const { toolCalls, toolResults } = steps[0]
-
-    expect(toolCalls[0].toolName).toBe('weather')
-    expect(toolCalls[0].args).toBe('{"location":"San Francisco"}')
-
-    expect(toolCalls[0].toolName).toBe('weather')
-    expect(toolResults[0].args).toStrictEqual({ location: 'San Francisco' })
-    expect(toolResults[0].result).toBe('{"location":"San Francisco","temperature":42}')
-  }, 20000)
+  })
 })
