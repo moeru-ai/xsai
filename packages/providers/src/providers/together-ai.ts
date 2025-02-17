6280b582209aa9dda94e3bebd356af077e7c7da1
Neko Ayaka <neko@ayaka.moe>
2025-02-17 15:24:41 +0800

    chore: remove
    Co-authored-by: 藍+85CD <50108258+kwaa@users.noreply.github.com>

--- packages/providers/src/providers/together-ai.ts
+++ packages/providers/src/providers/together-ai.ts
@@ -1,5 +1,3 @@
-import type { ListModelsResponse, Model } from '@xsai/model'
-
 import type { ChatProvider, EmbedProvider, ModelProvider, ProviderOptions, ProviderResult } from '../types'
 
 import { generateCRO } from '../utils/generate-cro'
@@ -7,7 +5,6 @@ import { generateCRO } from '../utils/generate-cro'
 export const createTogetherAI = (userOptions: ProviderOptions<true>):
   /** @see {@link https://api.together.ai/models} */
   ChatProvider<
-    | 'deepseek-ai/deepseek-llm-67b-chat'
     | 'deepseek-ai/DeepSeek-R1'
     | 'deepseek-ai/DeepSeek-R1-Distill-Llama-70B'
     | 'deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free'
@@ -20,19 +17,10 @@ export const createTogetherAI = (userOptions: ProviderOptions<true>):
     | 'meta-llama/Llama-3.3-70B-Instruct-Turbo'
     | 'meta-llama/Llama-3.3-70B-Instruct-Turbo-Free'
     | 'meta-llama/Llama-Vision-Free'
-    | 'microsoft/WizardLM-2-8x22B'
-    | 'mistralai/Mixtral-8x22B-Instruct-v0.1'
-    | 'NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO'
-    | 'nvidia/Llama-3.1-Nemotron-70B-Instruct-HF'
-    | 'Qwen/Qwen2-72B-Instruct'
-    | 'Qwen/Qwen2-VL-72B-Instruct'
     | 'Qwen/Qwen2.5-7B-Instruct-Turbo'
     | 'Qwen/Qwen2.5-72B-Instruct-Turbo'
     | 'Qwen/Qwen2.5-Coder-32B-Instruct'
     | 'Qwen/QwQ-32B-Preview'
-    | 'togethercomputer/m2-bert-80M-2k-retrieval'
-    | 'togethercomputer/m2-bert-80M-8k-retrieval'
-    | 'togethercomputer/m2-bert-80M-32k-retrieval'
   >
   & EmbedProvider<
     | 'BAAI/bge-base-en-v1.5'
@@ -54,12 +42,12 @@ export const createTogetherAI = (userOptions: ProviderOptions<true>):
         ...options,
         fetch: async (...args) => {
           return fetch(...args)
-            .then(async res => res.json() as Promise<Model[]>)
+            .then(async res => res.json() as Promise<any[]>)
             .then(models => Response.json(
               {
                 data: models,
                 object: 'list',
-              } satisfies ListModelsResponse,
+              },
             ))
         },
       }
