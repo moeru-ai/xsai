9aa0499809fde0f3b458c943b3caffe6a643561c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-20 19:51:23 +0800

    chore(xsschema): remove curly brackets

--- packages/xsschema/src/validate.ts
+++ packages/xsschema/src/validate.ts
@@ -6,9 +6,8 @@ export const validate = async <T extends StandardSchemaV1>(schema: T, input: Sta
   if (result instanceof Promise)
     result = await result
 
-  if (result.issues) {
+  if (result.issues)
     throw new Error(JSON.stringify(result.issues, null, 2))
-  }
 
   return (result as StandardSchemaV1.SuccessResult<T>).value
 }
