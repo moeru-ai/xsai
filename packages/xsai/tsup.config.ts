93b693a2504bcc26ed1e8ff5ba4c0bd9e62145ad
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-20 20:26:06 +0800

    refactor: use publish config with tsup

---
+++ packages/xsai/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup'
+
+import { default as defaults } from '../../tsup.config'
+
+export default defineConfig({
+  ...defaults,
+  entry: [
+    'src/index.ts',
+    'src/providers.ts'
+  ],
+})
