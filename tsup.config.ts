24a593bd4bce5793288ec52829aad7f5073168c9
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-20 20:31:30 +0800

    fix(tsup): set tsconfig

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,9 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
   dts: true,
   entry: ['src/index.ts'],
   format: 'esm',
+  tsconfig: resolve(import.meta.dirname, 'tsconfig.lib.json'),
 })
