01c82eb842a38856ee2b7b4afc162febd49d5018
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-20 22:51:52 +0800

    docs: new references

---
+++ docs/scripts/update-references.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @masknet/no-top-level */
+import { findWorkspaceDir } from '@pnpm/find-workspace-dir'
+import { glob, readFile, writeFile } from 'node:fs/promises'
+import { resolve } from 'node:path'
+import { cwd } from 'node:process'
+
+const workspaceDir = await findWorkspaceDir(cwd())
+
+const packages = glob(`${workspaceDir}/packages/**/package.json`)
+
+const pages = []
+
+for await (const pkg of packages) {
+  const { name } = JSON.parse(await readFile(pkg, 'utf8')) as { name: string }
+  pages.push(`[${name}](https://tsdocs.dev/search/docs/${name})`)
+}
+
+await writeFile(resolve(import.meta.dirname, '../content/docs/references/meta.json'), JSON.stringify({
+  defaultOpen: true,
+  pages: pages.toReversed(),
+  title: 'References',
+}, null, 2))
