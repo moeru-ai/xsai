bbf6fce950181efdc630fdecd4a35896f87787c9
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-21 13:12:23 +0800

    refactor(docs/references): include ext package

--- docs/scripts/update-references.ts
+++ docs/scripts/update-references.ts
@@ -4,13 +4,20 @@ import { glob, readFile, writeFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
 import { cwd } from 'node:process'
 
+const pages = []
+
 const workspaceDir = await findWorkspaceDir(cwd())
 
 const packages = glob(`${workspaceDir}/packages/**/package.json`)
 
-const pages = []
-
 for await (const pkg of packages) {
+  const { name } = JSON.parse(await readFile(pkg, 'utf8')) as { name: string }
+  pages.push(`[${name}](https://tsdocs.dev/search/docs/${name})`)
+}
+
+const packagesExt = glob(`${workspaceDir}/packages-ext/**/package.json`)
+
+for await (const pkg of packagesExt) {
   const { name } = JSON.parse(await readFile(pkg, 'utf8')) as { name: string }
   pages.push(`[${name}](https://tsdocs.dev/search/docs/${name})`)
 }
