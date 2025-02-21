a84b800ec36a87cada60cdddec0b8e155d05eae9
Neko Ayaka <neko@ayaka.moe>
2025-02-21 14:52:40 +0800

    feat(providers-cloud): add deepinfra

---
+++ packages-ext/providers-cloud/src/providers/deepinfra.ts
@@ -0,0 +1,41 @@
+import { createChatProvider, createEmbedProvider, createModelProvider, merge } from '@xsai-ext/shared-providers'
+
+/**
+ * [DeepInfra](https://deepinfra.com/) provider
+ *
+ * @see {@link https://deepinfra.com/pricing} and {@link https://deepinfra.com/docs/openai_api}
+ */
+export const createDeepInfra = (apiKey: string, baseURL = 'https://api.deepinfra.com/v1/openai/') => merge(
+  createChatProvider<
+    | '01-ai/Yi-34B-Chat'
+    | 'cognitivecomputations/dolphin-2.6-mixtral-8x7b'
+    | 'cognitivecomputations/dolphin-2.9.1-llama-3-70b'
+    | 'deepseek-ai/DeepSeek-R1'
+    | 'deepseek-ai/DeepSeek-R1-Distill-Llama-70B'
+    | 'deepseek-ai/DeepSeek-R1-Distill-Qwen-32B'
+    | 'deepseek-ai/DeepSeek-V3'
+    | 'google/gemma-2-9b-it'
+    | 'google/gemma-2-27b-it'
+    | 'meta-llama/Llama-3.3-70B-Instruct'
+    | 'meta-llama/Llama-3.3-70B-Instruct-Turbo'
+    | 'microsoft/phi-4'
+    | 'mistralai/Mixtral-8x7B-Instruct-v0.1'
+    | 'mistralai/Mixtral-8x22B-Instruct-v0.1'
+    | 'mistralai/Mixtral-8x22B-v0.1'
+    | 'Qwen/QVQ-72B-Preview'
+    | 'Qwen/Qwen2.5-7B-Instruct'
+    | 'Qwen/Qwen2.5-72B-Instruct'
+    | 'Qwen/Qwen2.5-Coder-7B'
+    | 'Qwen/Qwen2.5-Coder-32B-Instruct'
+    | 'Qwen/QwQ-32B-Preview'
+  >({ apiKey, baseURL }),
+  createModelProvider({ apiKey, baseURL }),
+  createEmbedProvider<
+    | 'bge-base-en-v1.5'
+    | 'bge-large-en-v1.5'
+    | 'e5-base-v2'
+    | 'e5-large-v2'
+    | 'gte-base'
+    | 'gte-large'
+  >({ apiKey, baseURL }),
+)
--- packages-ext/providers-cloud/src/providers/index.ts
+++ packages-ext/providers-cloud/src/providers/index.ts
@@ -1,3 +1,4 @@
+export { createDeepInfra } from './deepinfra'
 export { createDeepSeek } from './deepseek'
 export { createFireworks } from './fireworks'
 export { createGoogleGenerativeAI } from './google-generative-ai'
