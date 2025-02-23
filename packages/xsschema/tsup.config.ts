c4b400f4197f939903e8abab3cc9761e17488827
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-23 16:20:40 +0800

    fix(xsschema): pre-compile types

---
+++ packages/xsschema/tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsup'
+import defaults from '../../tsup.config'
+
+export default defineConfig({
+  ...defaults,
+  dts: {
+    resolve: ['@standard-schema/spec', 'json-schema']
+  }
+})
