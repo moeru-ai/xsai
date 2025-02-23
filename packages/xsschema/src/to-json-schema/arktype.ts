3aed72e0a6dbf637abb3944cde07162a6d570439
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-23 16:01:21 +0800

    chore(xsschema): clean

--- packages/xsschema/src/to-json-schema/arktype.ts
+++ packages/xsschema/src/to-json-schema/arktype.ts
@@ -1,10 +1,6 @@
 import type { Type } from 'arktype'
 import type { JSONSchema7 } from 'json-schema'
 
-export const toJsonSchema = async () => {
-  const toJsonSchema = (schema: unknown) =>
-    (schema as Type).toJsonSchema()
-
-  // eslint-disable-next-line @masknet/type-prefer-return-type-annotation
-  return toJsonSchema as (schema: unknown) => JSONSchema7
-}
+export const toJsonSchema = async () => 
+  (schema: unknown) =>
+    (schema as Type).toJsonSchema() as JSONSchema7
