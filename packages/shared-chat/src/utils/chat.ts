e52427efe56f3be4b827fe3754138c345da35c69
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-26 21:55:15 +0800

    feat(chat): update chat options

--- packages/shared-chat/src/utils/chat.ts
+++ packages/shared-chat/src/utils/chat.ts
@@ -4,10 +4,31 @@ import { requestBody, requestHeaders, requestURL, responseCatch } from '@xsai/sh
 
 import type { Message, Tool, ToolChoice } from '../types'
 
+/** @see {@link https://platform.openai.com/docs/api-reference/chat/create} */
 export interface ChatOptions extends CommonRequestOptions {
   [key: string]: unknown
+  /**
+   * number between -2.0 and 2.0.
+   * @default 0
+   */
+  frequencyPenalty?: number
   messages: Message[]
+  /**
+   * number between -2.0 and 2.0.
+   * @default 0
+   */
+  presencePenalty?: number
+  seed?: number
+  /** up to 4 sequences where the API will stop generating further tokens. */
+  stop?: [string, string, string, string ] | [string, string, string] | [string, string] | [string] | string
+  /**
+   * what sampling temperature to use, between 0 and 2.
+   * @default 1
+   */
+  temperature?: number
   toolChoice?: ToolChoice
+  /** @default 1 */
+  topP?: number
 }
 
 export const chat = async <T extends ChatOptions>(options: T) =>
