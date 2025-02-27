7ce1d395c2c8ca02e7e4d202e538c25c991cf3b5
Neko Ayaka <neko@ayaka.moe>
2025-02-27 13:34:35 +0800

    feat(embed|shared-provider): export EmbedResponse and extend more provider generics
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- packages-ext/shared-providers/src/utils/create.ts
+++ packages-ext/shared-providers/src/utils/create.ts
@@ -1,6 +1,6 @@
 import type { CreateProviderOptions } from '../types/create-provider-options'
 import type { ProviderMetadata } from '../types/metadata'
-import type { ChatProvider, ChatProviderWithExtraOptions, EmbedProvider, MetadataProviders, ModelProvider, SpeechProvider, SpeechProviderWithExtraOptions, TranscriptionProvider } from '../types/providers'
+import type { ChatProvider, ChatProviderWithExtraOptions, EmbedProvider, EmbedProviderWithExtraOptions, MetadataProviders, ModelProvider, ModelProviderWithExtraOptions, SpeechProvider, SpeechProviderWithExtraOptions, TranscriptionProvider, TranscriptionProviderWithExtraOptions } from '../types/providers'
 
 export const createChatProvider = <T extends string = string>(options: CreateProviderOptions): ChatProvider<T> => ({
   chat: model => Object.assign(options, { model }),
@@ -14,8 +14,16 @@ export const createEmbedProvider = <T extends string = string>(options: CreatePr
   embed: model => Object.assign(options, { model }),
 })
 
+export const createEmbedProviderWithExtraOptions = <T extends string = string, T2 = undefined>(options: CreateProviderOptions): EmbedProviderWithExtraOptions<T, T2> => ({
+  embed: (model, extraOptions) => Object.assign(options, { model }, extraOptions),
+})
+
 export const createModelProvider = (options: CreateProviderOptions): ModelProvider => ({
   model: () => options,
+})
+
+export const createModelProviderWithExtraOptions = <T = undefined>(options: Omit<CreateProviderOptions, 'model'>): ModelProviderWithExtraOptions<T> => ({
+  model: extraOptions => Object.assign(options, extraOptions),
 })
 
 export const createSpeechProvider = <T extends string = string>(options: CreateProviderOptions): SpeechProvider<T> => ({
@@ -28,6 +36,10 @@ export const createSpeechProviderWithExtraOptions = <T extends string = string, 
 
 export const createTranscriptionProvider = <T extends string = string>(options: CreateProviderOptions): TranscriptionProvider<T> => ({
   transcription: model => Object.assign(options, { model }),
+})
+
+export const createTranscriptionProviderWithExtraOptions = <T extends string = string, T2 = undefined>(options: CreateProviderOptions): TranscriptionProviderWithExtraOptions<T, T2> => ({
+  transcription: (model, extraOptions) => Object.assign(options, { model }, extraOptions),
 })
 
 export const createMetadataProvider = (id: string, otherMeta?: Omit<ProviderMetadata, 'id'>): MetadataProviders => ({
