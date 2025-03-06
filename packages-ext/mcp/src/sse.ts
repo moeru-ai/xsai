067148a2e48b713a6198ba6f0f0bbbaff5c3801d
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-03-06 16:43:28 +0800

    refactor(mcp): split tools and clients

--- packages-ext/mcp/src/sse.ts
+++ packages-ext/mcp/src/sse.ts
@@ -1,26 +1,23 @@
 import type { SSEClientTransportOptions } from '@modelcontextprotocol/sdk/client/sse.js'
-import type { Tool } from '@xsai/shared-chat'
 
 import { Client } from '@modelcontextprotocol/sdk/client/index.js'
 import { SSEClientTransport } from '@modelcontextprotocol/sdk/client/sse.js'
 
-import { mapTools } from './utils/map-tools'
-
-export type GetSSEServerToolsOptions = Record<string, SSEClientTransportOptions & {
+export type CreateSSEClientsOptions = Record<string, SSEClientTransportOptions & {
   url: URL
 }>
 
 /** @experimental */
-export const getSSEServerTools = async (mcpServers: GetSSEServerToolsOptions): Promise<Tool[]> => {
-  const result: Tool[] = []
+export const createSSEClients = async (mcpServers: CreateSSEClientsOptions, version: string = '1.0.0'): Promise<Record<string, Client>> =>
+  Object.fromEntries(
+    await Promise.all(
+      Object.entries(mcpServers)
+        .map(async ([serverName, { url, ...options }]) => {
+          const transport = new SSEClientTransport(url, options)
+          const client = new Client({ name: serverName, version })
+          await client.connect(transport)
 
-  for (const [serverName, { url, ...options }] of Object.entries(mcpServers)) {
-    const transport = new SSEClientTransport(url, options)
-    const client = new Client({ name: serverName, version: '1.0.0' })
-    await client.connect(transport)
-
-    result.push(...await mapTools(client, serverName))
-  }
-
-  return result
-}
+          return [serverName, client] as const
+        }),
+    ),
+  )
--- packages-ext/mcp/src/stdio.ts
+++ packages-ext/mcp/src/stdio.ts
@@ -1,26 +1,22 @@
 import type { StdioServerParameters } from '@modelcontextprotocol/sdk/client/stdio.js'
-import type { Tool } from '@xsai/shared-chat'
 
 import { Client } from '@modelcontextprotocol/sdk/client/index.js'
 import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio.js'
 import { stderr } from 'node:process'
 
-import { mapTools } from './utils/map-tools'
-
-export type GetStdioServerToolsOptions = Record<string, StdioServerParameters>
+export type CreateStdioClientsOptions = Record<string, StdioServerParameters>
 
 /** @experimental */
-export const getStdioServerTools = async (mcpServers: GetStdioServerToolsOptions): Promise<Tool[]> => {
-  const result: Tool[] = []
+export const createStdioClients = async (mcpServers: CreateStdioClientsOptions, version: string = '1.0.0'): Promise<Record<string, Client>> =>
+  Object.fromEntries(
+    await Promise.all(
+      Object.entries(mcpServers)
+        .map(async ([serverName, server]) => {
+          const transport = new StdioClientTransport({ ...server, stderr })
+          const client = new Client({ name: serverName, version })
+          await client.connect(transport)
 
-  /** @see {@link https://github.com/vercel/ai/issues/3891#issuecomment-2561348224} */
-  for (const [serverName, server] of Object.entries(mcpServers)) {
-    const transport = new StdioClientTransport({ ...server, stderr })
-    const client = new Client({ name: serverName, version: '1.0.0' })
-    await client.connect(transport)
-
-    result.push(...await mapTools(client, serverName))
-  }
-
-  return result
-}
+          return [serverName, client] as const
+        }),
+    ),
+  )
