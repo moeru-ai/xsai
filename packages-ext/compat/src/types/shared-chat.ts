d79383b93cdad232947bdbbc59ba7192f4d5c492
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-03-07 17:15:15 +0800

    chore(compat): update types

--- packages-ext/compat/src/types/shared-chat.ts
+++ packages-ext/compat/src/types/shared-chat.ts
@@ -1,3 +1,9 @@
+import type { AssistantMessagePart, UserMessagePart } from 'xsai'
+
+export type AssistantContent = Array<AssistantMessagePart> | string
+
+export type UserContent = Array<UserMessagePart> | string
+
 export type {
   AssistantMessage as CoreAssistantMessage,
   Message as CoreMessage,
@@ -5,3 +11,7 @@ export type {
   ToolMessage as CoreToolMessage,
   UserMessage as CoreUserMessage,
 } from 'xsai'
+
+// TODO:
+// ToolResultPart
+// ToolInvocation
