186e4254e540b8f7060e0eba6d4a8138815346e1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-03-08 18:57:46 +0800

    fix(compat/generate-text): export result type

--- packages-ext/compat/src/utils/generate-text.ts
+++ packages-ext/compat/src/utils/generate-text.ts
@@ -1,4 +1,4 @@
-import type { CommonRequestOptions, GenerateTextOptions as XSAIGenerateTextOptions, Tool as XSAITool } from 'xsai'
+import type { CommonRequestOptions, GenerateTextOptions as XSAIGenerateTextOptions, GenerateTextResult as XSAIGenerateTextResult, Tool as XSAITool } from 'xsai'
 
 import { generateText as xsaiGenerateText } from 'xsai'
 
@@ -8,12 +8,16 @@ import type { MessagesOrPrompts } from '../types/internal/messages-or-prompts'
 import { convertPrompts } from './internal/convert-prompts'
 import { convertTools } from './internal/convert-tools'
 
+// @ts-expect-error unused generics
+// eslint-disable-next-line unused-imports/no-unused-vars
+export interface GenerateTextResult<T1 = undefined, T2 = undefined> extends XSAIGenerateTextResult {}
+
 type GenerateTextOptions = MessagesOrPrompts & Omit<XSAIGenerateTextOptions, 'tools' | keyof CommonRequestOptions> & {
   model: LanguageModel
   tools?: Record<string, () => Promise<XSAITool>>
 }
 
-export const generateText = async (options: GenerateTextOptions) => xsaiGenerateText({
+export const generateText = async (options: GenerateTextOptions): Promise<GenerateTextResult> => xsaiGenerateText({
   ...options,
   ...options.model,
   messages: options.messages ?? convertPrompts(options.prompt!, options.system),
