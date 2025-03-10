7bf38147f2a688f886f0c1d049a415cf9020c14d
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-03-10 14:36:23 +0800

    fix(compat/stream-text): export result type

--- packages-ext/compat/src/utils/stream-text.ts
+++ packages-ext/compat/src/utils/stream-text.ts
@@ -1,4 +1,4 @@
-import type { CommonRequestOptions, StreamTextOptions as XSAIStreamTextOptions, Tool as XSAITool } from 'xsai'
+import type { CommonRequestOptions, StreamTextOptions as XSAIStreamTextOptions, StreamTextResult as XSAIStreamTextResult, Tool as XSAITool } from 'xsai'
 
 import { streamText as xsaiStreamText } from 'xsai'
 
@@ -7,6 +7,10 @@ import type { MessagesOrPrompts } from '../types/internal/messages-or-prompts'
 
 import { convertPrompts } from './internal/convert-prompts'
 import { convertTools } from './internal/convert-tools'
+
+// @ts-expect-error unused generics
+// eslint-disable-next-line unused-imports/no-unused-vars
+export interface StreamTextResult<T1 = undefined, T2 = undefined> extends XSAIStreamTextResult {}
 
 type StreamTextOptions = MessagesOrPrompts & Omit<XSAIStreamTextOptions, 'tools' | keyof CommonRequestOptions> & {
   model: LanguageModel
