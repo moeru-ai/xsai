9eb7bb74ca4b12d868edd2a932afe5d7763a7239
Menci 💖 <menci@microsoft.com>
2025-03-10 18:33:39 +0800

    feat(providers-cloud): add azure

---
+++ packages-ext/providers-cloud/src/providers/azure.ts
@@ -0,0 +1,182 @@
+import {
+  createChatProvider,
+  createEmbedProvider,
+  createMetadataProvider,
+  createModelProvider,
+  createSpeechProvider,
+  createTranscriptionProvider,
+  merge,
+} from '@xsai-ext/shared-providers'
+
+export interface AzureOptions {
+  /**
+   * The Azure AD access token fetcher for authorization.
+   *
+   * Either `apiKey` or `accessTokenFetcher` must be provided.
+   */
+  accessTokenFetcher?: () => Promise<string>
+  /**
+   * The static Azure API key for authorization.
+   *
+   * Either `apiKey` or `accessTokenFetcher` must be provided.
+   */
+  apiKey?: string
+  /**
+   * The Azure API version to use (`api-version` param).
+   *
+   * @default '2024-05-01-preview'
+   */
+  apiVersion?: string
+  /**
+   * The base URL (usually ends with `/models`, without `/chat/completions`) of your Azure AI endpoint.
+   *
+   * @note It's not recommended to use the `/openai/deployments/<deployment_name>/` endpoint since it ignores the `model` parameter passed through the request body. If you got an endpoint like that just replace the pathname with `/models`.
+   *
+   * @example 'https://<instance>.services.ai.azure.com/models'
+   * @example 'https://<instance>.cognitiveservices.azure.com/models'
+   */
+  baseURL: string
+}
+
+/**
+ * For Azure AI services, you can have multiple deployments of the same model with different names.
+ *
+ * Please pass your deployment name as the `model` parameter. By default, when you're deploying a model, Azure will use the model name as the deployment name.
+ *
+ * @see {@link https://ai.azure.com/explore/models}
+ */
+export const createAzure = (options: AzureOptions) => {
+  const baseURL = options.baseURL
+  const apiVersion = options.apiVersion ?? '2024-05-01-preview'
+  const apiKey = options.apiKey
+  const accessTokenFetcher = options.accessTokenFetcher
+  if (apiKey == null && !accessTokenFetcher) {
+    throw new Error('Either `apiKey` or `accessTokenFetcher` must be provided')
+  }
+
+  const fetch: typeof globalThis.fetch = async (input, init) => {
+    // If the input is a string, it is the URL, otherwise it is an object with a url property
+    const inputIsURL = typeof input === 'string' || !('url' in input)
+
+    // Add the api-version query parameter to the URL
+    const url = new URL(inputIsURL ? input : input.url)
+    url.searchParams.set('api-version', apiVersion)
+    const urlString = url.toString()
+
+    // Add credential header
+    const credentialHeaderKey = apiKey != null ? 'api-key' : 'Authorization'
+    const credentialHeaderValue = apiKey ?? `Bearer ${await accessTokenFetcher!()}`
+    init ??= {}
+    init.headers ??= {}
+    if (Array.isArray(init.headers)) {
+      init.headers.push([credentialHeaderKey, credentialHeaderValue])
+    }
+    else if (init.headers instanceof globalThis.Headers) {
+      init.headers.append(credentialHeaderKey, credentialHeaderValue)
+    }
+    else {
+      init.headers[credentialHeaderKey] = credentialHeaderValue
+    }
+
+    return globalThis.fetch(inputIsURL ? urlString : { ...input, url: urlString }, init)
+  }
+
+  return merge(
+    createMetadataProvider('azure'),
+    createChatProvider<AzureChatModels>({ apiKey, baseURL, fetch }),
+    createEmbedProvider<AzureTextEmbeddingModels>({ apiKey, baseURL, fetch }),
+    createSpeechProvider<AzureSpeechModels>({ apiKey, baseURL, fetch }),
+    createTranscriptionProvider<AzureTranscriptionModels>({ apiKey, baseURL, fetch }),
+    createModelProvider({ apiKey, baseURL, fetch }),
+  )
+}
+
+export type AzureChatModels =
+  | 'AI21-Jamba-1.5-Large'
+  | 'AI21-Jamba-1.5-Mini'
+  | 'ALLaM-2-7b-instruct'
+  | 'Cerence-CaLLM-Edge'
+  | 'Codestral-2501'
+  | 'Cohere-command-r'
+  | 'Cohere-command-r-08-2024'
+  | 'Cohere-command-r-plus'
+  | 'Cohere-command-r-plus-08-2024'
+  | 'databricks-dbrx-instruct'
+  | 'DeepSeek-R1'
+  | 'DeepSeek-R1-Distilled-NPU-Optimized'
+  | 'DeepSeek-V3'
+  | 'E.L.Y.Crop-Protection'
+  | 'financial-reports-analysis'
+  | 'gpt-4'
+  | 'gpt-4-32k'
+  | 'gpt-4.5-preview'
+  | 'gpt-4o'
+  | 'gpt-4o-mini'
+  | 'gpt-35-turbo'
+  | 'gpt-35-turbo-16k'
+  | 'gpt-35-turbo-instruct'
+  | 'Gretel-Navigator-Tabular'
+  | 'jais-30b-chat'
+  | 'Llama-2-7b-chat'
+  | 'Llama-2-13b-chat'
+  | 'Llama-2-70b-chat'
+  | 'Llama-3.2-1B-Instruct'
+  | 'Llama-3.2-3B-Instruct'
+  | 'Llama-3.2-11B-Vision-Instruct'
+  | 'Llama-3.2-90B-Vision-Instruct'
+  | 'Llama-3.3-70B-Instruct'
+  | 'Llama-Guard-3-1B'
+  | 'Llama-Guard-3-8B'
+  | 'Llama-Guard-3-11B-Vision'
+  | 'Meta-Llama-3-8B-Instruct'
+  | 'Meta-Llama-3-70B-Instruct'
+  | 'Meta-Llama-3.1-8B-Instruct'
+  | 'Meta-Llama-3.1-70B-Instruct'
+  | 'Meta-Llama-3.1-405B-Instruct'
+  | 'Ministral-3B'
+  | 'Mistral-large'
+  | 'Mistral-large-2407'
+  | 'Mistral-Large-2411'
+  | 'Mistral-Nemo'
+  | 'Mistral-small'
+  | 'mistralai-Mistral-7B-Instruct-v0-2'
+  | 'mistralai-Mistral-7B-Instruct-v01'
+  | 'mistralai-Mixtral-8x7B-Instruct-v01'
+  | 'mistralai-Mixtral-8x22B-Instruct-v0-1'
+  | 'o1'
+  | 'o1-mini'
+  | 'o1-preview'
+  | 'o3-mini'
+  | 'Phi-3-medium-4k-instruct'
+  | 'Phi-3-medium-128k-instruct'
+  | 'Phi-3-mini-4k-instruct'
+  | 'Phi-3-mini-128k-instruct'
+  | 'Phi-3-small-8k-instruct'
+  | 'Phi-3-small-128k-instruct'
+  | 'Phi-3-vision-128k-instruct'
+  | 'Phi-3.5-mini-instruct'
+  | 'Phi-3.5-MoE-instruct'
+  | 'Phi-3.5-vision-instruct'
+  | 'Phi-4'
+  | 'Phi-4-mini-instruct'
+  | 'Phi-4-multimodal-instruct'
+  | 'RA-FT-Optix-Food-Beverage'
+  | 'snowflake-arctic-instruct'
+  | 'supply-chain-trade-regulations'
+  | 'tsuzumi-7b'
+
+export type AzureSpeechModels =
+  | 'tts'
+  | 'tts-hd'
+
+export type AzureTextEmbeddingModels =
+  | 'Cohere-embed-v3-english'
+  | 'Cohere-embed-v3-multilingual'
+  | 'text-embedding-3-large'
+  | 'text-embedding-3-small'
+  | 'text-embedding-ada-002'
+
+export type AzureTranscriptionModels =
+  | 'openai-whisper-large'
+  | 'openai-whisper-large-v3'
+  | 'whisper'
