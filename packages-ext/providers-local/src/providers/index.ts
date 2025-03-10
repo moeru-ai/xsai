ac48b2290c6ed59e896debce0a165ff23b1032fb
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-03-10 15:55:26 +0800

    chore(providers-local): remove claude2openai

--- packages-ext/providers-local/src/providers/index.ts
+++ packages-ext/providers-local/src/providers/index.ts
@@ -1,4 +1,3 @@
-export { createClaude2OpenAI } from './claude2openai'
 export { createOllama } from './ollama'
 export { createUnSpeech } from './unspeech'
 export { createUnElevenLabs } from './unspeech/elevenlabs'
