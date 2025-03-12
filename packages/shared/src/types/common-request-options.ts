d1fb5b2ae11da311771150c3a7d49a215edf013c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-03-12 20:37:43 +0800

    refactor: simplify fetch type (#104)

--- packages/shared/src/types/common-request-options.ts
+++ packages/shared/src/types/common-request-options.ts
@@ -1,9 +1,11 @@
+import type { Fetch } from './fetch'
+
 export interface CommonRequestOptions {
   abortSignal?: AbortSignal
   apiKey?: string
   /** @example `https://api.openai.com/v1/` */
   baseURL: string | URL
-  fetch?: typeof globalThis.fetch
+  fetch?: Fetch | typeof globalThis.fetch
   /** @default `undefined` */
   headers?: Headers | Record<string, string>
   /** @example `gpt-4o` */
--- packages/shared/src/types/index.ts
+++ packages/shared/src/types/index.ts
@@ -1,1 +1,2 @@
 export type { CommonRequestOptions } from './common-request-options'
+export type { Fetch } from './fetch'
