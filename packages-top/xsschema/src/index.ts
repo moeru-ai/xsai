af3a6e7bfb1b0d441850b3154f4d4e777329b608
Travis Fischer <fisch0920@gmail.com>
2025-03-21 19:56:58 +0800

    fix: missing sync exports

--- packages-top/xsschema/src/index.ts
+++ packages-top/xsschema/src/index.ts
@@ -1,4 +1,4 @@
-export { toJsonSchema, toJsonSchema as toJSONSchema } from './to-json-schema'
+export { toJsonSchema, toJsonSchema as toJSONSchema, toJsonSchemaSync, toJsonSchemaSync as toJSONSchemaSync } from './to-json-schema'
 export type { Infer, InferIn, JSONSchema as JsonSchema, JSONSchema, Schema, StandardSchemaVendor } from './types'
 export { validate } from './validate'
 export { registerStandardSchemaVendor, registerStandardSchemaVendors } from './vendors'
