9286d5e68a73642f1a437b3b3384ae479a46ecde
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-03-24 21:59:52 +0800

    chore(tool): update test

--- packages/tool/test/index.test.ts
+++ packages/tool/test/index.test.ts
@@ -1,4 +1,4 @@
-import { description, object, pipe, string } from 'valibot'
+import { description, number, object, pipe, string } from 'valibot'
 import { describe, expect, it } from 'vitest'
 
 import { tool } from '../src'
@@ -36,6 +36,67 @@ describe('@xsai/tool', () => {
       },
       required: [
         'location',
+      ],
+      type: 'object',
+    })
+  })
+
+  it('weather with returns', async () => {
+    const name = 'weather'
+    const desc = 'Get the weather in a location'
+
+    const weather = await tool({
+      description: desc,
+      execute: ({ location }) => ({
+        location,
+        temperature: 42,
+      }),
+      name,
+      parameters: object({
+        location: pipe(
+          string(),
+          description('The location to get the weather for'),
+        ),
+      }),
+      returns: object({
+        location: pipe(
+          string(),
+          description('The location to get the weather for'),
+        ),
+        temperature: number(),
+      }),
+    })
+
+    expect(weather.type).toBe('function')
+    expect(weather.function.name).toBe(name)
+    expect(weather.function.description).toBe(desc)
+    expect(weather.function.parameters).toStrictEqual({
+      $schema: 'http://json-schema.org/draft-07/schema#',
+      properties: {
+        location: {
+          description: 'The location to get the weather for',
+          type: 'string',
+        },
+      },
+      required: [
+        'location',
+      ],
+      type: 'object',
+    })
+    expect(weather.function.returns).toStrictEqual({
+      $schema: 'http://json-schema.org/draft-07/schema#',
+      properties: {
+        location: {
+          description: 'The location to get the weather for',
+          type: 'string',
+        },
+        temperature: {
+          type: 'number',
+        },
+      },
+      required: [
+        'location',
+        'temperature',
       ],
       type: 'object',
     })
