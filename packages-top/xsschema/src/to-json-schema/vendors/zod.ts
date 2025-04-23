91ea343738830a1b6ea06b80d7c49dd6ce201020
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-04-23 19:21:26 +0800

    feat(xsschema): support zod 4 beta, zod mini (#132)
    * feat(xsschema): support zod 4 beta, zod mini
    
    * docs(xsschema): update coverage
    
    * fix: lint errors

--- packages-top/xsschema/src/to-json-schema/vendors/zod.ts
+++ packages-top/xsschema/src/to-json-schema/vendors/zod.ts
@@ -1,11 +1,45 @@
-import type { JSONSchema7 } from 'json-schema'
 import type { ZodSchema } from 'zod'
+import type { ZodType } from 'zod4'
 
 import type { ToJsonSchemaFn } from '.'
 
-import { tryImport } from '.'
-
 export const getToJsonSchemaFn = async (): Promise<ToJsonSchemaFn> => {
-  const { zodToJsonSchema } = await tryImport(import('zod-to-json-schema'), 'zod-to-json-schema')
-  return schema => zodToJsonSchema(schema as ZodSchema<unknown>) as JSONSchema7
+  let toJSONSchema: ToJsonSchemaFn = (_schema: unknown) => {
+    throw new Error('xsschema: Missing dependencies "zod" or "@zod/mini".')
+  }
+  let zodToJSONSchema: ToJsonSchemaFn = (_schema: unknown) => {
+    throw new Error('xsschema: Missing dependencies "zod-to-json-schema".')
+  }
+
+  try {
+    const { z } = await import('@zod/mini')
+    toJSONSchema = z.toJSONSchema as ToJsonSchemaFn
+  }
+  catch {}
+
+  try {
+    const { z } = await import('zod')
+    if ('toJSONSchema' in z)
+      toJSONSchema = z.toJSONSchema as ToJsonSchemaFn
+  }
+  catch (err) {
+    if (err instanceof Error)
+      console.error(err.message)
+  }
+
+  try {
+    const { zodToJsonSchema } = await import('zod-to-json-schema')
+    zodToJSONSchema = zodToJsonSchema as ToJsonSchemaFn
+  }
+  catch (err) {
+    if (err instanceof Error)
+      console.error(err.message)
+  }
+
+  return async (schema: unknown) => {
+    if ('_zod' in (schema as ZodSchema | ZodType))
+      return toJSONSchema(schema)
+    else
+      return zodToJSONSchema(schema)
+  }
 }
