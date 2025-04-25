b7bb25ce7df45a0de68f887012f9ca96cf13f4e4
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-04-25 16:45:04 +0800

    chore: fix lint warning

--- packages/shared/src/utils/response-json.ts
+++ packages/shared/src/utils/response-json.ts
@@ -1,11 +1,12 @@
 import { responseCatch } from './response-catch'
 
-export const responseJSON = async <T>(res: Response) =>
+export const responseJSON = async <T>(res: Response): Promise<T> =>
   responseCatch(res)
     .then(async (res) => {
       const text = await res.text()
 
       try {
+        // eslint-disable-next-line @masknet/type-prefer-return-type-annotation
         return JSON.parse(text) as T
       }
       catch {
