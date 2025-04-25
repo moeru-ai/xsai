5c2349b144f8c62a040c1418a57fb84f66292df2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-04-25 16:49:22 +0800

    refactor(shared): remove cause, included to message
    closed #119

--- packages/shared/src/utils/response-catch.ts
+++ packages/shared/src/utils/response-catch.ts
@@ -2,16 +2,14 @@ import { XSAIError } from '../error'
 
 export const responseCatch = async (res: Response) => {
   if (!res.ok) {
-    const error = new XSAIError(`Remote sent ${res.status} response`, res)
-    error.cause = new Error(await res.text())
+    const error = new XSAIError(`Remote sent ${res.status} response: ${await res.text()}`, res)
     throw error
   }
   if (!res.body) {
     throw new XSAIError('Response body is empty from remote server', res)
   }
   if (!(res.body instanceof ReadableStream)) {
-    const error = new XSAIError(`Expected Response body to be a ReadableStream, but got ${String(res.body)}`, res)
-    error.cause = new Error(`Content-Type is ${res.headers.get('Content-Type')}`)
+    const error = new XSAIError(`Expected Response body to be a ReadableStream, but got ${String(res.body)}; Content Type is ${res.headers.get('Content-Type')}`, res)
     throw error
   }
 
