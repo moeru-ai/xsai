fcf222d8351963006ef6d713ae5ac39ddd972f51
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-04-25 16:52:24 +0800

    chore(shared): simplify

--- packages/shared/src/utils/response-catch.ts
+++ packages/shared/src/utils/response-catch.ts
@@ -1,17 +1,12 @@
 import { XSAIError } from '../error'
 
 export const responseCatch = async (res: Response) => {
-  if (!res.ok) {
-    const error = new XSAIError(`Remote sent ${res.status} response: ${await res.text()}`, res)
-    throw error
-  }
-  if (!res.body) {
+  if (!res.ok)
+    throw new XSAIError(`Remote sent ${res.status} response: ${await res.text()}`, res)
+  if (!res.body)
     throw new XSAIError('Response body is empty from remote server', res)
-  }
-  if (!(res.body instanceof ReadableStream)) {
-    const error = new XSAIError(`Expected Response body to be a ReadableStream, but got ${String(res.body)}; Content Type is ${res.headers.get('Content-Type')}`, res)
-    throw error
-  }
+  if (!(res.body instanceof ReadableStream))
+    throw new XSAIError(`Expected Response body to be a ReadableStream, but got ${String(res.body)}; Content Type is ${res.headers.get('Content-Type')}`, res)
 
   return res
 }
