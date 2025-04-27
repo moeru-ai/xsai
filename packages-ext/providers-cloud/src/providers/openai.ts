7f65158bca36c2dc66fc8864a7ed3c8712bb5c3f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-04-27 14:08:01 +0800

    feat(providers-cloud/openai): add image provider

--- packages-ext/providers-cloud/src/providers/openai.ts
+++ packages-ext/providers-cloud/src/providers/openai.ts
@@ -1,6 +1,7 @@
 import {
   createChatProvider,
   createEmbedProvider,
+  createImageProvider,
   createMetadataProvider,
   createModelProvider,
   createSpeechProvider,
@@ -12,6 +13,7 @@ export const createOpenAI = (apiKey: string, baseURL = 'https://api.openai.com/v
   createMetadataProvider('openai'),
   createChatProvider<'gpt-4o' | 'gpt-4o-mini' | 'o1-mini' | 'o1-preview'>({ apiKey, baseURL }),
   createEmbedProvider<'text-embedding-3-large' | 'text-embedding-3-small'>({ apiKey, baseURL }),
+  createImageProvider<'dall-e-2' | 'dall-e-3' | 'gpt-image-1'>({ apiKey, baseURL }),
   createModelProvider({ apiKey, baseURL }),
   createSpeechProvider<'tts-1' | 'tts-1-hd'>({ apiKey, baseURL }),
   createTranscriptionProvider<'whisper-1'>({ apiKey, baseURL }),
