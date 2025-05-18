6036e9ff9e55c26cdc3c3cb7c849533f4d3d21fd
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-05-18 22:24:43 +0800

    chore(generate-transcription): use speeches for test

--- packages/generate-transcription/test/index.test.ts
+++ packages/generate-transcription/test/index.test.ts
@@ -7,13 +7,14 @@ describe('@xsai/generate-transcription', () => {
   it('basic', async () => {
     const { text } = await generateTranscription({
       apiKey: 'a',
-      baseURL: new URL('http://localhost:9010'),
+      baseURL: new URL('http://localhost:8000/v1/'),
       file: await openAsBlob('./test/fixtures/basic.wav', { type: 'audio/wav' }),
       fileName: 'basic.wav',
-      language: 'en-US',
-      model: 'ggml-large-v3-turbo-q5_0.bin',
+      // language: 'en-US',
+      language: 'en',
+      model: 'deepdml/faster-whisper-large-v3-turbo-ct2',
     })
 
     expect(text).toBe('Hello, I am your AI assistant. Just let me know how I can help bring your ideas to life.')
-  })
+  }, 30000)
 })
