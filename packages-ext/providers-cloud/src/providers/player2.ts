27042232b084d2a80c2b25273362c98b5d6c2723
lorisj <lorisjautakas@gmail.com>
2025-06-10 18:00:25 -0400

    add player2 as a provider

--- packages-ext/providers-cloud/src/providers/player2.ts
+++
@@ -1,9 +0,0 @@
-import {
-  createChatProvider,
-  createMetadataProvider,
-  createSpeechProvider,
-  createTranscriptionProvider,
-  merge,
-} from '@xsai-ext/shared-providers'
-
-export const createPlayer2 = (baseURL = 'http://localhost:4315/v1/') => merge(createMetadataProvider('player2-api'), createChatProvider({ baseURL }), createSpeechProvider({ baseURL }), createTranscriptionProvider({ baseURL }))
