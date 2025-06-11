35795fc67fd8a83b4d4d1e18ed2f78ad3143e920
Loris Jautakas <59267852+lorisj@users.noreply.github.com>
2025-06-11 11:40:25 -0400

    feat(providers-local): add player2 (#163)
    * add player 2
    
    * add player2 as a provider
    
    * add to index file
    
    * 'player2-api' -> 'player2'
    
    ---------
    
    Co-authored-by: 藍+85CD <50108258+kwaa@users.noreply.github.com>

---
+++ packages-ext/providers-local/src/providers/player2.ts
@@ -0,0 +1,7 @@
+import {
+  createChatProvider,
+  createMetadataProvider,
+  merge,
+} from '@xsai-ext/shared-providers'
+
+export const createPlayer2 = (baseURL = 'http://localhost:4315/v1/') => merge(createMetadataProvider('player2'), createChatProvider({ baseURL }))
