e4482ab0f8f8bd5ad8abe8e48500f43b973eefb5
lorisj <lorisjautakas@gmail.com>
2025-06-13 12:43:58 -0400

    removed special tag, added new fetch to player2 instead

--- packages-ext/providers-local/src/providers/player2.ts
+++ packages-ext/providers-local/src/providers/player2.ts
@@ -5,4 +5,42 @@ import {
   merge,
 } from '@xsai-ext/shared-providers'
 
-export const createPlayer2 = (baseURL = 'http://localhost:4315/v1/') => merge(createMetadataProvider('player2'), createChatProvider({ baseURL }), createSpeechProvider({ baseURL, specialTag: 'player2' }))
+export const createPlayer2 = (baseURL = 'http://localhost:4315/v1/') => merge(createMetadataProvider('player2'), createChatProvider({ baseURL }), createSpeechProvider({
+  baseURL,
+  fetch: async (input: Parameters<typeof globalThis.fetch>[0], reqInit: Parameters<typeof globalThis.fetch>[1]) => {
+    const newUrl = input.toString().replace('audio/speech', 'tts/speak')
+    if (typeof reqInit?.body === 'string') {
+      try {
+        const original: unknown = JSON.parse(reqInit.body)
+        if (typeof original === 'object' && original != null) {
+          const { input, responseFormat, speed, voice, ...rest } = original as {
+            input?: string
+            responseFormat?: 'aac' | 'flac' | 'mp3' | 'opus' | 'pcm' | 'wav'
+            rest: object
+            speed?: number
+            voice?: string
+          }
+          const modified = {
+            ...original,
+            audio_format: responseFormat,
+            play_in_app: false,
+            speed: speed ?? 1.0,
+            text: input,
+            voice_ids:
+              voice !== undefined
+              && voice != null
+                ? [voice]
+                : [],
+            ...rest,
+          }
+          reqInit.body = JSON.stringify(modified)
+        }
+      }
+      catch (err) {
+        console.warn('Could not parse body as JSON:', err)
+      }
+    }
+
+    return globalThis.fetch(newUrl, reqInit)
+  },
+}))
--- packages/generate-speech/src/index.ts
+++ packages/generate-speech/src/index.ts
@@ -12,32 +12,8 @@ export interface GenerateSpeechOptions extends CommonRequestOptions {
   voice: string
 }
 
-export const generateSpeech = async (options: GenerateSpeechOptions): Promise<ArrayBuffer> => {
-  if (options.specialTag === 'player2') {
-    // special processing for player2, namely use tts/speak, also rename input -> text, voice_ids -> voice, etc. so that it matches the API
-    const { input, responseFormat, speed, voice, ...rest } = options
-    return (options.fetch ?? globalThis.fetch)(requestURL('tts/speak', options.baseURL), {
-      body: requestBody({
-        audio_format: responseFormat,
-        play_in_app: false,
-        speed: speed ?? 1.0,
-        text: input,
-        voice_ids: voice
-          ? []
-          : [
-              voice,
-            ],
-        ...rest,
-      }),
-      headers: requestHeaders({
-        'Content-Type': 'application/json',
-        ...options.headers,
-      }, options.apiKey),
-      method: 'POST',
-      signal: options.abortSignal,
-    }).then(responseCatch).then(async res => res.arrayBuffer())
-  }
-  return (options.fetch ?? globalThis.fetch)(requestURL('audio/speech', options.baseURL), {
+export const generateSpeech = async (options: GenerateSpeechOptions): Promise<ArrayBuffer> =>
+  (options.fetch ?? globalThis.fetch)(requestURL('audio/speech', options.baseURL), {
     body: requestBody(options),
     headers: requestHeaders({
       'Content-Type': 'application/json',
@@ -48,4 +24,3 @@ export const generateSpeech = async (options: GenerateSpeechOptions): Promise<Ar
   })
     .then(responseCatch)
     .then(async res => res.arrayBuffer())
-}
