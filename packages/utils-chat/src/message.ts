c4a6685ad6e4a97f083f698eba3f9cf41c47e0a9
Acbox <acbox0328@gmail.com>
2025-06-17 21:25:02 +0800

    feat(utils-chat): Add value inserts for message utils

--- packages/utils-chat/src/message.ts
+++ packages/utils-chat/src/message.ts
@@ -1,14 +1,44 @@
 import type { AssistantMessage, AssistantMessagePart, ImagePart, Message, SystemMessage, SystemMessagePart, TextPart, ToolCall, ToolMessage, ToolMessagePart, UserMessage, UserMessagePart } from '@xsai/shared-chat'
 
+export interface InsertResolver {
+  parts: PartsInsertResolver
+  string: StringInsertResolver
+}
+export type PartsInsertResolver = (content: TextPart[], inserts: Record<string, string>) => TextPart[]
+export type StringInsertResolver = (content: string, inserts: Record<string, string>) => string
+
+const replaceInString: StringInsertResolver = (content, inserts) => {
+  const regex = /\{\{([^{}]+)\}\}/g
+  return content.replace(regex, (match, p1: string) => inserts[p1] || match)
+}
+
+const replaceInParts: PartsInsertResolver = (content, inserts) => {
+  const regex = /\{\{([^{}]+)\}\}/g
+  return content.map(part => ({ ...part, text: part.text.replace(regex, (match, p1: string) => inserts[p1] || match) }))
+}
+
+export const defaultInsertResolver: InsertResolver = {
+  parts: replaceInParts,
+  string: replaceInString,
+}
+
 export const messages = (...messages: Message[]): Message[] => messages
 
-export const system = <C extends string | SystemMessagePart[]>(content: C): SystemMessage => ({ content, role: 'system' })
-
-export const user = <C extends Array<UserMessagePart> | string>(content: C): UserMessage => ({ content, role: 'user' })
-
-export const textPart = (text: string): TextPart => ({ text, type: 'text' })
+export const textPart = (text: string, inserts: Record<string, string> = {}, resolver: StringInsertResolver = defaultInsertResolver.string): TextPart => ({ text: resolver(text, inserts), type: 'text' })
 
 export const imagePart = (url: string): ImagePart => ({ image_url: { url }, type: 'image_url' })
+
+export const system = <C extends string | SystemMessagePart[]>(
+  content: C,
+  inserts: Record<string, string> = {},
+  resolver: InsertResolver = defaultInsertResolver,
+): SystemMessage => ({ content: typeof content === 'string' ? resolver.string(content, inserts) : resolver.parts(content, inserts), role: 'system' })
+
+export const user = <C extends Array<UserMessagePart> | string>(
+  content: C,
+  inserts: Record<string, string> = {},
+  resolver: InsertResolver = defaultInsertResolver,
+): UserMessage => ({ content: typeof content === 'string' ? resolver.string(content, inserts) : resolver.parts(content.filter(part => part.type === 'text'), inserts), role: 'user' })
 
 export const isToolCall = (content: AssistantMessagePart[] | string | ToolCall | ToolCall[]): content is ToolCall | ToolCall[] => {
   const isElementToolCallLike = (c: AssistantMessagePart | AssistantMessagePart[] | string | ToolCall | ToolCall[]) => {
@@ -26,9 +56,10 @@ export const isToolCall = (content: AssistantMessagePart[] | string | ToolCall |
 
 export const assistant = <C extends AssistantMessagePart[] | string | ToolCall | ToolCall[]>(content: C): AssistantMessage => {
   if (isToolCall(content)) {
-    return Array.isArray(content)
-      ? { role: 'assistant', tool_calls: content }
-      : { role: 'assistant', tool_calls: [content] }
+    return {
+      role: 'assistant',
+      tool_calls: Array.isArray(content) ? content : [content],
+    }
   }
 
   return { content, role: 'assistant' }
