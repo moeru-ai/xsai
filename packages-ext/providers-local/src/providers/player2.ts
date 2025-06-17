a73b8d4e9c72051652ad735811c89602e10ce930
lorisj <lorisjautakas@gmail.com>
2025-06-17 08:54:56 -0400

    minor changes

--- packages-ext/providers-local/src/providers/player2.ts
+++ packages-ext/providers-local/src/providers/player2.ts
@@ -9,37 +9,30 @@ import { Buffer } from 'node:buffer'
 export const createPlayer2 = (baseURL = 'http://localhost:4315/v1/') => merge(createMetadataProvider('player2'), createChatProvider({ baseURL }), createSpeechProvider({
   baseURL,
   fetch: async (input: Parameters<typeof globalThis.fetch>[0], reqInit: Parameters<typeof globalThis.fetch>[1]) => {
-    const newUrl = input.toString().replace('audio/speech', 'tts/speak')
-    if (typeof reqInit?.body === 'string') {
-      try {
-        const original: unknown = JSON.parse(reqInit.body)
-        if (typeof original === 'object' && original != null) {
-          const { input, responseFormat, speed, voice, ...rest } = original as {
-            input?: string
-            responseFormat?: 'aac' | 'flac' | 'mp3' | 'opus' | 'pcm' | 'wav'
-            rest: object
-            speed?: number
-            voice?: string
-          }
-          const modified = {
-            ...original,
-            audio_format: responseFormat,
-            play_in_app: false,
-            speed: speed ?? 1.0,
-            text: input,
-            voice_ids:
-                      voice !== undefined
-                      && voice != null
-                        ? [voice]
-                        : [],
-            ...rest,
-          }
-          reqInit.body = JSON.stringify(modified)
-        }
+    const newUrl = `${input.toString().slice(0, -'audio/speech'.length)}tts/speak`
+    try {
+      const { input, response_format, speed, voice, ...rest } = JSON.parse(reqInit?.body as string) as {
+        input?: string
+        response_format?: 'aac' | 'flac' | 'mp3' | 'opus' | 'pcm' | 'wav'
+        rest: object
+        speed?: number
+        voice?: string
       }
-      catch (err) {
-        console.warn('Could not parse body as JSON:', err)
+      const modified = {
+        audio_format: response_format,
+        play_in_app: false,
+        speed: speed ?? 1.0,
+        text: input,
+        voice_ids: voice != null
+          ? [voice]
+          : [],
+        ...rest,
       }
+      if (reqInit)
+        reqInit.body = JSON.stringify(modified)
+    }
+    catch (err) {
+      console.warn('Could not parse body as JSON:', err)
     }
     return globalThis.fetch(newUrl, reqInit).then(async res => res.json() as Promise<{ data?: string }>).then((json: { data?: string }) => {
       const base64 = json.data ?? ''
