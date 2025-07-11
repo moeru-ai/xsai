a5340d5645ee83f5896f231b6654171b9bbfdf03
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-07-11 14:58:53 +0800

    chore(model): use `granite3.3:2b` for test

--- packages/model/test/index.test.ts
+++ packages/model/test/index.test.ts
@@ -3,26 +3,28 @@ import { describe, expect, it } from 'vitest'
 import { listModels, retrieveModel } from '../src'
 
 describe('@xsai/model', () => {
+  const model = 'granite3.3:2b'
+
   it('listModels', async () => {
     const models = await listModels({
       baseURL: 'http://localhost:11434/v1/',
     })
 
-    const llama = models.find(({ id }) => id === 'llama3.2:latest')!
+    const granite = models.find(({ id }) => id === model)!
 
-    expect(llama.id).toBe('llama3.2:latest')
-    expect(llama.object).toBe('model')
-    expect(llama.owned_by).toBe('library')
+    expect(granite.id).toBe(model)
+    expect(granite.object).toBe('model')
+    expect(granite.owned_by).toBe('library')
   })
 
   it('retrieveModel', async () => {
-    const llama = await retrieveModel({
+    const granite = await retrieveModel({
       baseURL: 'http://localhost:11434/v1/',
-      model: 'llama3.2:latest',
+      model,
     })
 
-    expect(llama.id).toBe('llama3.2:latest')
-    expect(llama.object).toBe('model')
-    expect(llama.owned_by).toBe('library')
+    expect(granite.id).toBe(model)
+    expect(granite.object).toBe('model')
+    expect(granite.owned_by).toBe('library')
   })
 })
