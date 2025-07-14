fc6b3bc47ebc08c2e08264f853d386af903a0091
LemonNeko <chheese048@gmail.com>
2025-07-14 21:45:47 +0800

    fix: split minimax global and minimax china

--- packages-ext/providers-cloud/src/providers/minimax.ts
+++ packages-ext/providers-cloud/src/providers/minimax.ts
@@ -1,7 +1,14 @@
 import { createChatProvider, createMetadataProvider, merge } from '@xsai-ext/shared-providers'
 
-/** @see {@link https://www.minimax.io/platform/document/platform%20introduction} */
-export const createMinimax = (apiKey: string, baseURL = 'https://api.minimaxi.chat/v1/') => merge(
+/** @see {@link https://www.minimax.io/platform/document/platform%20introduction?key=66701c8e1d57f38758d58198} */
+export const createMinimaxGlobal = (apiKey: string, baseURL = 'https://api.minimaxi.chat/v1/') => merge(
+  createMetadataProvider('minimax'),
+  /** @see {@link https://www.minimax.io/platform/document/ChatCompletion%20v2?key=66701d281d57f38758d581d0#QklxsNSbaf6kM4j6wjO5eEek} */
+  createChatProvider<'abab6.5s-chat' | 'DeepSeek-R1' | 'MiniMax-Text-01'>({ apiKey, baseURL }),
+)
+
+/** @see {@link https://platform.minimaxi.com/document/platform%20introduction?key=66701c8e1d57f38758d58198} */
+export const createMinimaxChina = (apiKey: string, baseURL = 'https://api.minimax.chat/v1/') => merge(
   createMetadataProvider('minimax'),
   /** @see {@link https://platform.minimaxi.com/document/ChatCompletion%20v2?key=66701d281d57f38758d581d0#1XspWaYA7baUnFix0otJIQkt} */
   createChatProvider<'abab6.5s-chat' | 'DeepSeek-R1' | 'MiniMax-Text-01'>({ apiKey, baseURL }),
