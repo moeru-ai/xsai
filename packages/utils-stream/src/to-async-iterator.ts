3a4e33cd9feba1381ee229012a316f412834a168
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-07-23 22:02:53 +0800

    refactor(utils-stream): export from std

--- packages/utils-stream/src/to-async-iterator.ts
+++ packages/utils-stream/src/to-async-iterator.ts
@@ -1,21 +1,2 @@
-// TODO: update @moeru/std
-// export { readableStreamToAsyncIterator as toAsyncIterator } from '@moeru/std/async-iterator'
-/** @see {@link https://developer.mozilla.org/en-US/docs/Web/API/ReadableStream#browser_compatibility} */
-export async function* toAsyncIterator<T>(
-  stream: ReadableStream<T>,
-): AsyncGenerator<T, void, unknown> {
-  const reader = stream.getReader()
-  try {
-    while (true) {
-      const { done, value } = await reader.read()
-
-      if (done)
-        return
-
-      yield value
-    }
-  }
-  finally {
-    reader.releaseLock()
-  }
-}
+// TODO: deprecate this
+export { readableStreamToAsyncIterator as toAsyncIterator } from '@moeru/std/async-iterator'
