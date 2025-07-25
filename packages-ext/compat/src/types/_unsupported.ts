d79b2d07ad68f360cb4c4349c09f2feb9f5b70ea
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-07-25 22:47:09 +0800

    refactor!: remove compat package
    closed #87

--- packages-ext/compat/src/types/_unsupported.ts
+++
@@ -1,2 +0,0 @@
-/* eslint-disable sonarjs/redundant-type-aliases */
-export type TelemetrySettings = any
--- packages-ext/compat/src/utils/_unsupported.ts
+++
@@ -1,9 +0,0 @@
-export const raw = (providers: unknown) => providers
-export {
-  raw as customProvider,
-  raw as experimental_customProvider,
-  raw as jsonSchema,
-  raw as Output,
-  raw as valibotSchema,
-  raw as zodSchema,
-}
