805cbf82cafd5d21f6fd2559c221604cddc8248a
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-07-31 20:59:08 +0800

    fix(xsschema): fix strict json schema in nested object

--- packages-top/xsschema/src/json-schema.ts
+++ packages-top/xsschema/src/json-schema.ts
@@ -5,4 +5,15 @@ export const jsonSchema = (schema: JsonSchema) => schema
 export const strictJsonSchema = (schema: JsonSchema): JsonSchema => ({
   ...schema,
   additionalProperties: false,
+  properties: schema.properties
+    ? Object.fromEntries(
+        Object.entries(schema.properties)
+          .map(([k, v]) => [
+            k,
+            typeof v === 'object' && 'type' in v && v.type === 'object'
+              ? strictJsonSchema(v)
+              : v,
+          ]),
+      )
+    : schema.properties,
 })
