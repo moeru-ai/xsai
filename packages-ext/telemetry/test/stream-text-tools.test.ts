2fbbf3d6ce0cbcbeac277c74edb63139a1337388
Makito <i@maki.to>
2025-09-04 14:52:46 +0900

    feat: telemetry support for stream-text (#213)

--- packages-ext/telemetry/test/stream-text-tools.test.ts
+++ packages-ext/telemetry/test/stream-text-tools.test.ts
@@ -1,28 +1,46 @@
+import type { AttributeValue } from '@opentelemetry/api'
 import type { ReadableSpan } from '@opentelemetry/sdk-trace-base'
 
 import { InMemorySpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base'
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
-import { generateText as aiGenerateText, tool as aiTool, stepCountIs } from 'ai'
+import { streamText as aiStreamText, tool as aiTool, stepCountIs } from 'ai'
 import { ollama } from 'ollama-ai-provider-v2'
 import { describe, expect, it } from 'vitest'
 import { tool } from 'xsai'
 import { z } from 'zod/v4'
 
-import { generateText } from '../src'
+import { streamText } from '../src'
 
-describe.sequential('generateText with tools', () => {
+describe.sequential('streamText with tools', () => {
   const memoryExporter = new InMemorySpanExporter()
   const tracerProvider = new NodeTracerProvider({
     spanProcessors: [new SimpleSpanProcessor(memoryExporter)],
   })
   tracerProvider.register()
 
+  const trimmedAttributes = new Set<string>([
+    // Not using the following because we are testing with --update
+    // 'ai.response.avgOutputTokensPerSecond', // reason: time
+    // 'ai.response.id', // reason: id
+    // 'ai.response.msToFinish', // reason: time
+    // 'ai.response.msToFirstChunk', // reason: time
+    // 'ai.response.timestamp', // reason: time
+    // 'ai.response.toolCalls', // reason: contains id
+    // 'ai.toolCall.id', // reason: id
+    // 'gen_ai.response.id', // reason: id
+  ])
+
   const getAttributes = (span: ReadableSpan) => Object.fromEntries(
     Object.entries(span.attributes)
-      .map(([k, v]) => k === 'ai.response.text'
-        ? [k, (v as string).replaceAll('\n', ' ').slice(0, 50)]
-        : [k, v],
-      ),
+      .reduce<[string, AttributeValue | undefined][]>((entries, [k, v]) => {
+        if (k === 'ai.response.text') {
+          entries.push([k, (v as string).replaceAll('\n', ' ').slice(0, 50)])
+        }
+        else if (!trimmedAttributes.has(k)) {
+          entries.push([k, v])
+        }
+        return entries
+      }, []),
   )
 
   it('basic', async () => {
@@ -38,21 +56,31 @@ describe.sequential('generateText with tools', () => {
       }),
     })
 
-    const { text } = await generateText({
+    let text = ''
+    const { textStream } = streamText({
       baseURL: 'http://localhost:11434/v1',
+      maxSteps: 5,
       messages: [{
         content: 'How many times does 114514 plus 1919810 equal? Please try to call the `add` tool to solve the problem.',
         role: 'user',
       }],
       model: 'qwen3:0.6b',
+      onFinish: async () => {
+        const spans = memoryExporter.getFinishedSpans().map(getAttributes)
+
+        expect(text).toMatchSnapshot()
+        expect(spans).toMatchSnapshot()
+      },
       seed: 114514,
+      streamOptions: {
+        includeUsage: true,
+      },
       tools: [add],
     })
 
-    const spans = memoryExporter.getFinishedSpans().map(getAttributes)
-
-    expect(text).toMatchSnapshot()
-    expect(spans).toMatchSnapshot()
+    for await (const textDelta of textStream) {
+      text += textDelta
+    }
   }, 120_000)
 
   it('basic/ai', async () => {
@@ -70,7 +98,7 @@ describe.sequential('generateText with tools', () => {
       name: 'add',
     })
 
-    const { text } = await aiGenerateText({
+    const { text } = aiStreamText({
       experimental_telemetry: { isEnabled: true },
       model: ollama('qwen3:0.6b'),
       prompt: 'How many times does 114514 plus 1919810 equal? Please try to call the `add` tool to solve the problem.',
@@ -79,9 +107,10 @@ describe.sequential('generateText with tools', () => {
       tools: { add },
     })
 
+    const fullText = await text
     const spans = memoryExporter.getFinishedSpans().slice(4).map(getAttributes)
 
-    expect(text).toMatchSnapshot()
+    expect(fullText).toMatchSnapshot()
     expect(spans).toMatchSnapshot()
   }, 120_000)
 
