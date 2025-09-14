617ab49dfa3451a12ca655f05cc9b63ff489894b
Rizumu Ayaka <rizumu@ayaka.moe>
2025-09-14 17:18:56 +0800

    fix: lint

--- packages-ext/telemetry/src/utils/stream-text.ts
+++ packages-ext/telemetry/src/utils/stream-text.ts
@@ -182,7 +182,8 @@ export const streamText = (options: WithTelemetry<StreamTextOptions>) => {
 
     if (tool_calls.length !== 0) {
       for (const toolCall of tool_calls) {
-        if (!toolCall) continue
+        if (!toolCall)
+          continue
         const { completionToolCall, completionToolResult, message } = await executeTool({
           abortSignal: options.abortSignal,
           messages,
--- packages/stream-text/src/index.ts
+++ packages/stream-text/src/index.ts
@@ -164,7 +164,8 @@ export const streamText = (options: StreamTextOptions): StreamTextResult => {
 
     if (tool_calls.length !== 0) {
       for (const toolCall of tool_calls) {
-        if (!toolCall) continue
+        if (!toolCall)
+          continue
         const { completionToolCall, completionToolResult, message } = await executeTool({
           abortSignal: options.abortSignal,
           messages,
