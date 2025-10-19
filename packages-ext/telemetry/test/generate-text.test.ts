2d0a35b066f967214488bf049346ff207d885f89
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-10-19 17:15:18 +0800

    chore(telemetry): disable compare test

--- packages-ext/telemetry/test/generate-text.test.ts
+++ packages-ext/telemetry/test/generate-text.test.ts
@@ -2,8 +2,8 @@ import type { ReadableSpan } from '@opentelemetry/sdk-trace-base'
 
 import { InMemorySpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base'
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
-import { generateText as aiGenerateText } from 'ai'
-import { ollama } from 'ollama-ai-provider-v2'
+// import { generateText as aiGenerateText } from 'ai'
+// import { ollama } from 'ollama-ai-provider-v2'
 import { describe, expect, it } from 'vitest'
 
 import { generateText } from '../src'
@@ -46,38 +46,38 @@ describe.sequential('generateText', () => {
     expect(spans).toMatchSnapshot()
   }, 120_000)
 
-  it('basic/ai', async () => {
-    const { text } = await aiGenerateText({
-      experimental_telemetry: {
-        isEnabled: true,
-        metadata: {
-          agentId: 'weather-assistant',
-          instructions: 'You are a helpful weather assistant',
-        },
-      },
-      model: ollama('qwen3:0.6b'),
-      prompt: 'Why is the sky blue?',
-      seed: 114514,
-    })
+  // it('basic/ai', async () => {
+  //   const { text } = await aiGenerateText({
+  //     experimental_telemetry: {
+  //       isEnabled: true,
+  //       metadata: {
+  //         agentId: 'weather-assistant',
+  //         instructions: 'You are a helpful weather assistant',
+  //       },
+  //     },
+  //     model: ollama('qwen3:0.6b'),
+  //     prompt: 'Why is the sky blue?',
+  //     seed: 114514,
+  //   })
 
-    const spans = memoryExporter.getFinishedSpans().slice(2).map(getAttributes)
+  //   const spans = memoryExporter.getFinishedSpans().slice(2).map(getAttributes)
 
-    expect(text).toMatchSnapshot()
-    expect(spans).toMatchSnapshot()
-  }, 120_000)
+  //   expect(text).toMatchSnapshot()
+  //   expect(spans).toMatchSnapshot()
+  // }, 120_000)
 
-  it('basic/compare', () => {
-    const extractAttributeKeys = (span: ReadableSpan) => Object.keys(span.attributes)
-      // eslint-disable-next-line sonarjs/no-nested-functions
-      .filter(key => ['ai.settings'].every(prefix => !key.startsWith(prefix)))
-      .sort((a, b) => a.localeCompare(b))
+  // it('basic/compare', () => {
+  //   const extractAttributeKeys = (span: ReadableSpan) => Object.keys(span.attributes)
+  //     // eslint-disable-next-line sonarjs/no-nested-functions
+  //     .filter(key => ['ai.settings'].every(prefix => !key.startsWith(prefix)))
+  //     .sort((a, b) => a.localeCompare(b))
 
-    const spans = memoryExporter.getFinishedSpans()
-    const xsai = spans.slice(0, 2).map(extractAttributeKeys)
-    const ai = spans.slice(2).map(extractAttributeKeys)
+  //   const spans = memoryExporter.getFinishedSpans()
+  //   const xsai = spans.slice(0, 2).map(extractAttributeKeys)
+  //   const ai = spans.slice(2).map(extractAttributeKeys)
 
-    xsai.forEach((attributes, i) => {
-      expect(attributes).toStrictEqual(ai[i])
-    })
-  })
+  //   xsai.forEach((attributes, i) => {
+  //     expect(attributes).toStrictEqual(ai[i])
+  //   })
+  // })
 })
