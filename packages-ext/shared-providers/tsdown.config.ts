050bfe57208cb8407ed174f19955fb1ddd9e7003
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-10-31 20:12:20 +0800

    chore: lint code

--- packages-ext/shared-providers/tsdown.config.ts
+++ packages-ext/shared-providers/tsdown.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'tsdown'
+
 import pkg from './package.json' with { type: 'json' }
 
 export default defineConfig({
-  entry: 'src/index.ts',
   dts: {
     build: true,
-    resolve: Object.keys(pkg.devDependencies)
-  }
+    resolve: Object.keys(pkg.devDependencies),
+  },
+  entry: 'src/index.ts',
 })
