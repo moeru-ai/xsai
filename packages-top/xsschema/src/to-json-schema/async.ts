33d2c4473af9f153a2d4839f35c6b7168ad1c5dd
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-12-20 20:06:52 +0800

    feat(xsschema): support standard json schema v1

--- packages-top/xsschema/src/to-json-schema/async.ts
+++ packages-top/xsschema/src/to-json-schema/async.ts
@@ -1,4 +1,4 @@
-import type { StandardSchemaV1 } from '@standard-schema/spec'
+import type { StandardJSONSchemaV1, StandardSchemaV1 } from '@standard-schema/spec'
 import type { JSONSchema7 } from 'json-schema'
 
 import { getToJsonSchemaFn } from './vendors'
@@ -8,6 +8,14 @@ import { getToJsonSchemaFn } from './vendors'
  *
  * @note This method is `async` because it has to `await import` the schema vendor's dependencies.
  */
-export const toJsonSchema = async (schema: StandardSchemaV1): Promise<JSONSchema7> =>
-  getToJsonSchemaFn(schema['~standard'].vendor)
-    .then(async toJsonSchema => toJsonSchema(schema))
+export const toJsonSchema = async (schema: StandardSchemaV1): Promise<JSONSchema7> => {
+  // eslint-disable-next-line @masknet/type-no-force-cast-via-top-type
+  if ('jsonSchema' in schema['~standard'] && 'input' in (schema as unknown as StandardJSONSchemaV1)['~standard'].jsonSchema) {
+    // eslint-disable-next-line @masknet/type-no-force-cast-via-top-type
+    return (schema as unknown as StandardJSONSchemaV1)['~standard'].jsonSchema.input({ target: 'draft-07' })
+  }
+  else {
+    const toJsonSchema = await getToJsonSchemaFn(schema['~standard'].vendor)
+    return toJsonSchema(schema)
+  }
+}
