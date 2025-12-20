8805cd3ce175a321a5596fd70eb31237c1db6deb
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-12-20 20:19:29 +0800

    feat(xsschema)!: support standard json schema v1 (#238)
    * chore(deps): bump standard schema spec to 1.1.0
    
    * chore(deps): bump schema dev deps
    
    * feat(xsschema): support standard json schema v1
    
    * chore(xsschema): update test
    
    * refactor(xsschema): improve structure
    
    * chore: remove bracket

--- packages-top/xsschema/src/to-json-schema/async.ts
+++ packages-top/xsschema/src/to-json-schema/async.ts
@@ -1,6 +1,7 @@
 import type { StandardSchemaV1 } from '@standard-schema/spec'
 import type { JSONSchema7 } from 'json-schema'
 
+import { isStandardJSONSchemaV1 } from '../utils/is-standard-json-schema-v1'
 import { getToJsonSchemaFn } from './vendors'
 
 /**
@@ -9,5 +10,7 @@ import { getToJsonSchemaFn } from './vendors'
  * @note This method is `async` because it has to `await import` the schema vendor's dependencies.
  */
 export const toJsonSchema = async (schema: StandardSchemaV1): Promise<JSONSchema7> =>
-  getToJsonSchemaFn(schema['~standard'].vendor)
-    .then(async toJsonSchema => toJsonSchema(schema))
+  isStandardJSONSchemaV1(schema)
+    ? schema['~standard'].jsonSchema.input({ target: 'draft-07' })
+    : getToJsonSchemaFn(schema['~standard'].vendor)
+        .then(async toJsonSchema => toJsonSchema(schema))
