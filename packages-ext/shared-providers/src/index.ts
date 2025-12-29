482345cd5c9d5f986a0b9cd952d0c419d8dbe6da
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-12-29 22:44:42 +0800

    refactor!: remove old providers package

--- packages-ext/shared-providers/src/index.ts
+++
@@ -1,5 +0,0 @@
-export type * from './types/create-provider-options'
-export type * from './types/metadata'
-export type * from './types/providers'
-export * from './utils/create'
-export * from './utils/merge'
