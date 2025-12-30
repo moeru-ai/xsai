af41c03a723442d4b04075451ebdc25b3805ba41
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-12-30 14:22:28 +0800

    refactor(xsschema)!: remove sync method (#254)
    * refactor(xsschema)!: remove sync method
    
    * chore(xsschema): update test
    
    * fix(xsschema): update exports
    
    * docs(xsschema): update

--- packages-top/xsschema/src/to-json-schema/index.ts
+++ packages-top/xsschema/src/to-json-schema/index.ts
@@ -1,2 +1,16 @@
-export { toJsonSchema } from './async'
-export { initToJsonSchemaSyncVendor, toJsonSchemaSync } from './sync'
+import type { StandardSchemaV1 } from '@standard-schema/spec'
+import type { JSONSchema7 } from 'json-schema'
+
+import { isStandardJSONSchemaV1 } from '../utils/is-standard-json-schema-v1'
+import { getToJsonSchemaFn } from './vendors'
+
+/**
+ * Converts a Standard Schema to a JSON schema.
+ *
+ * @note This method is `async` because it has to `await import` the schema vendor's dependencies.
+ */
+export const toJsonSchema = async (schema: StandardSchemaV1): Promise<JSONSchema7> =>
+  isStandardJSONSchemaV1(schema)
+    ? schema['~standard'].jsonSchema.input({ target: 'draft-07' })
+    : getToJsonSchemaFn(schema['~standard'].vendor)
+        .then(async toJsonSchema => toJsonSchema(schema))
