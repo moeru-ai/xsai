1687be8bb433cda02a200fab3666064385351cff
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-01-26 16:43:20 +0800

    feat: execute tool

--- packages-ext/responses/src/utils/responses.ts
+++ packages-ext/responses/src/utils/responses.ts
@@ -3,9 +3,9 @@ import type { OpenResponsesOptions } from '../types/open-responses-options'
 import type { StreamingEvent } from '../types/streaming-event'
 
 import { requestBody, requestHeaders, requestURL, responseCatch, trampoline } from '@xsai/shared'
-import { signal } from 'alien-signals'
 import { EventSourceParserStream } from 'eventsource-parser/stream'
 
+import { executeTool } from './execute-tool'
 import { normalizeInput } from './normalize-input'
 import { normalizeOutput } from './normalize-output'
 import { StreamingEventParserStream } from './streaming-event-parser-stream'
@@ -19,8 +19,8 @@ export interface ResponsesOptions extends OpenResponsesOptions {
 }
 
 export const responses = (options: ResponsesOptions) => {
-  const input = signal(normalizeInput(structuredClone(options.input)))
-  const steps = signal<ResponseCompletedStreamingEvent[]>([])
+  const input = normalizeInput(structuredClone(options.input))
+  const steps: ResponseCompletedStreamingEvent[] = []
 
   // output
   let textCtrl: ReadableStreamDefaultController<string> | undefined
@@ -32,7 +32,7 @@ export const responses = (options: ResponsesOptions) => {
     const res = await (options.fetch ?? globalThis.fetch)(requestURL('responses', options.baseURL), {
       body: requestBody({
         ...options,
-        input: input(),
+        input,
         stream: true,
         tools: options.tools?.map(({ execute, ...tool }) => tool),
       }),
@@ -53,7 +53,7 @@ export const responses = (options: ResponsesOptions) => {
           textCtrl?.error(reason)
         },
         close: () => {},
-        write: (event) => {
+        write: async (event) => {
           // eslint-disable-next-line no-console
           console.log(event)
 
@@ -61,13 +61,25 @@ export const responses = (options: ResponsesOptions) => {
 
           // eslint-disable-next-line ts/switch-exhaustiveness-check
           switch (event.type) {
-            // case 'response.function_call_arguments.done':
-
-            //   break
             case 'response.completed':
-              steps([...steps(), event])
-              input([...input(), ...normalizeOutput(event.response.output)])
+              steps.push(event)
               break
+            case 'response.output_item.done': {
+              if (event.item == null)
+                break
+
+              input.push(...normalizeOutput([event.item]))
+
+              if (event.item.type === 'function_call') {
+                const functionCallOutput = await executeTool({
+                  functionCall: event.item,
+                  tools: options.tools,
+                })
+                input.push(...normalizeOutput([functionCallOutput]))
+              }
+
+              break
+            }
             case 'response.output_text.delta':
               textCtrl?.enqueue(event.delta)
               break
@@ -76,6 +88,10 @@ export const responses = (options: ResponsesOptions) => {
           }
         },
       }))
+
+    // TODO: stopWhen
+    if (input.at(-1)?.type === 'function_call_output')
+      return async () => doStream()
   }
 
   void (async () => {
