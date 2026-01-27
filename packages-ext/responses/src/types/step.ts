b4b3fb34a72f231b2afe9ece2dda6193a2ddbaa4
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-01-27 19:41:25 +0800

    refactor: return steps

--- packages-ext/responses/src/types/step.ts
+++ packages-ext/responses/src/types/step.ts
@@ -5,7 +5,7 @@ export interface Step {
   functionCallOutputs?: FunctionCallOutput[]
   functionCalls?: FunctionCall[]
   response: ResponseResource
-  usage: Usage
+  usage?: Usage
   // TODO: text
   // TODO: reasoning
   // TODO: finishReason
