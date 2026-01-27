26eb736947103e2340d225aafb485c6eecc0c2d0
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-01-27 18:47:07 +0800

    chore: step type

---
+++ packages-ext/responses/src/types/step.ts
@@ -0,0 +1,12 @@
+import type { FunctionCall, FunctionCallOutput, ResponseResource } from '../generated'
+import type { Usage } from './usage'
+
+export interface Step {
+  functionCallOutputs?: FunctionCallOutput[]
+  functionCalls?: FunctionCall[]
+  response: ResponseResource
+  usage: Usage
+  // TODO: text
+  // TODO: reasoning
+  // TODO: finishReason
+}
