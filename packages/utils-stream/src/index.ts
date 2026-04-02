acbf637650c79ec4e823578c5a12a63f8eb4e27b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-04-02 21:20:12 +0800

    refactor(utils-stream)!: remove to async iterator

--- packages/utils-stream/src/index.ts
+++ packages/utils-stream/src/index.ts
@@ -1,3 +1,2 @@
 export { simulateReadableStream, type SimulateReadableStreamOptions } from './simulate-readable-stream'
 export { smoothStream, type SmoothStreamOptions } from './smooth-stream'
-export { toAsyncIterator } from './to-async-iterator'
