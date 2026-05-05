e0bffa2f7f1e57e61afae4092745e59227dd44f5
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-05-05 18:15:56 +0800

    refactor(responses): add event types

---
+++ packages-ext/responses/src/types/event.ts
@@ -0,0 +1,79 @@
+// import type { CompletionToolCall, CompletionToolResult, FinishReason, Usage } from '@xsai/shared-chat'
+
+import type { ErrorPayload, ItemField } from '../generated'
+import type { Usage } from './usage'
+
+export interface ErrorEvent {
+  error: ErrorPayload
+  type: 'error'
+}
+
+export interface ReasoningDeltaEvent {
+  delta: string
+  type: 'reasoning.delta'
+}
+
+export interface ReasoningDoneEvent {
+  text: string
+  type: 'reasoning.done'
+}
+
+export interface ReasoningStartEvent {
+  outputIndex: number
+  type: 'reasoning.start'
+}
+
+export interface StepDoneEvent {
+  // TODO: reason
+  output: ItemField[]
+  type: 'step.done'
+  usage: Usage
+}
+
+export interface StepStartEvent {
+  type: 'step.start'
+}
+
+export interface TextDeltaEvent {
+  delta: string
+  type: 'text.delta'
+}
+
+export interface TextDoneEvent {
+  text: string
+  type: 'text.done'
+}
+
+export interface TextStartEvent {
+  outputIndex: number
+  type: 'text.start'
+}
+
+export interface ToolCallDeltaEvent {
+  delta: string
+  type: 'tool-call.delta'
+}
+
+export interface ToolCallDoneEvent {
+  toolCall: {
+    arguments: string
+    id: string
+    name: string
+  }
+  type: 'tool-call.done'
+}
+
+export interface ToolCallStartEvent {
+  outputIndex: number
+  type: 'tool-call.start'
+}
+
+export interface ToolResultDoneEvent {
+  toolResult: {
+    id: string
+    input: unknown
+    name: string
+    output: unknown
+  }
+  type: 'tool-result.done'
+}
