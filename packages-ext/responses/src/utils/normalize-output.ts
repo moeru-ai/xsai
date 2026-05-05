7ebae24dea7a6549960f59b633c6e70a62c49d8d
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-05-05 18:42:54 +0800

    fix(responses): support compaction

--- packages-ext/responses/src/utils/normalize-output.ts
+++ packages-ext/responses/src/utils/normalize-output.ts
@@ -1,9 +1,11 @@
-import type { AssistantMessageItemParam, DeveloperMessageItemParam, FunctionCallItemParam, FunctionCallOutputItemParam, ItemField, ItemParam, ReasoningItemParam, SystemMessageItemParam, UserMessageItemParam } from '../generated'
+import type { AssistantMessageItemParam, CompactionSummaryItemParam, DeveloperMessageItemParam, FunctionCallItemParam, FunctionCallOutputItemParam, ItemField, ItemParam, ReasoningItemParam, SystemMessageItemParam, UserMessageItemParam } from '../generated'
 
 export const normalizeOutput = (output: ItemField[]): ItemParam[] =>
   // eslint-disable-next-line array-callback-return
   output.map((item) => {
     switch (item.type) {
+      case 'compaction':
+        return item as CompactionSummaryItemParam
       case 'function_call':
         return item as FunctionCallItemParam
       case 'function_call_output':
