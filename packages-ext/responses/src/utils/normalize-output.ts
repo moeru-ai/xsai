ee3a4f578ec6960f16bcd73a9cf9d190b1ce1129
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-05-08 16:23:48 +0800

    feat(responses): return input

--- packages-ext/responses/src/utils/normalize-output.ts
+++ packages-ext/responses/src/utils/normalize-output.ts
@@ -1,18 +1,16 @@
 import type { AssistantMessageItemParam, CompactionSummaryItemParam, DeveloperMessageItemParam, FunctionCallItemParam, FunctionCallOutputItemParam, ItemField, ItemParam, ReasoningItemParam, SystemMessageItemParam, UserMessageItemParam } from '../generated'
 
-export const normalizeOutput = (output: ItemField[]): ItemParam[] =>
-  // eslint-disable-next-line array-callback-return
-  output.map((item) => {
-    switch (item.type) {
-      case 'compaction':
-        return item as CompactionSummaryItemParam
-      case 'function_call':
-        return item as FunctionCallItemParam
-      case 'function_call_output':
-        return item as FunctionCallOutputItemParam
-      case 'message':
-        return item as AssistantMessageItemParam | DeveloperMessageItemParam | SystemMessageItemParam | UserMessageItemParam
-      case 'reasoning':
-        return item as ReasoningItemParam
-    }
-  })
+export const normalizeOutput = (item: ItemField): ItemParam => {
+  switch (item.type) {
+    case 'compaction':
+      return item as CompactionSummaryItemParam
+    case 'function_call':
+      return item as FunctionCallItemParam
+    case 'function_call_output':
+      return item as FunctionCallOutputItemParam
+    case 'message':
+      return item as AssistantMessageItemParam | DeveloperMessageItemParam | SystemMessageItemParam | UserMessageItemParam
+    case 'reasoning':
+      return item as ReasoningItemParam
+  }
+}
