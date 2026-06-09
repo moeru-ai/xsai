3d2f33b2eae53f87d15d416a476772f5c2d28836
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-06-09 15:47:29 +0800

    feat: stream speech, update speech options

--- packages/generate-speech/src/index.ts
+++ packages/generate-speech/src/index.ts
@@ -3,11 +3,23 @@ import type { CommonRequestOptions, WithUnknown } from '@xsai/shared'
 import { postJSON } from '@xsai/shared'
 
 export interface GenerateSpeechOptions extends CommonRequestOptions {
+  /** The text to generate audio for.  */
   input: string
-  /** @default `mp3` */
+  /** Control the voice of your generated audio with additional instructions. */
+  instructions?: string
+  /**
+   * The format to audio in.
+   * @default `mp3`
+   */
   responseFormat?: 'aac' | 'flac' | 'mp3' | 'opus' | 'pcm' | 'wav'
-  /** @default `1.0` */
+  /**
+   * The speed of the generated audio.
+   * @default 1.0
+   */
   speed?: number
+  /** If you want to enable stream, use `@xsai/stream-speech`. */
+  streamFormat?: never
+  /** The voice to use when generating the audio. */
   voice: string
 }
 
