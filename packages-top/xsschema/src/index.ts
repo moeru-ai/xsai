e233bf42162d5bd67ffc6edf615cd401747b890f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-07-05 13:36:18 +0800

    feat(tool): define tool (#312)

--- packages-top/xsschema/src/index.ts
+++ packages-top/xsschema/src/index.ts
@@ -1,4 +1,4 @@
 export { jsonSchema, strictJsonSchema } from './json-schema'
 export { toJsonSchema } from './to-json-schema'
-export type { Infer, InferIn, JsonSchema, Schema } from './types'
+export type { Infer, InferIn, JsonSchema, Schema, SchemaWithJson } from './types'
 export { validate } from './validate'
--- packages/tool/src/index.ts
+++ packages/tool/src/index.ts
@@ -1,2 +1,3 @@
+export { defineTool, type DefineToolOptions } from './utils/define-tool'
 export { rawTool, type RawToolOptions } from './utils/raw-tool'
 export { tool, type ToolOptions } from './utils/tool'
