6f5739d421dec129c02883cccfa5304b627d3b9c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-09-11 18:17:56 +0800

    refactor: split tool, executable tool

--- packages/text-primitives/src/core/tool.ts
+++ packages/text-primitives/src/core/tool.ts
@@ -2,9 +2,12 @@ import type { StandardJSONSchemaV1, StandardSchemaV1 } from '@standard-schema/sp
 
 import type { ToolResultPartContent } from './types/content'
 
+export interface ExecutableTool extends Tool {
+  execute: (input: unknown) => Promise<string | ToolResultPartContent[]> | string | ToolResultPartContent[]
+}
+
 export interface Tool {
   description?: string
-  execute?: (input: unknown) => Promise<string | ToolResultPartContent[]> | string | ToolResultPartContent[]
   inputSchema: Record<string, unknown>
   name: string
   outputSchema?: Record<string, unknown>
@@ -13,7 +16,7 @@ export interface Tool {
 
 export interface ToolOptions<TInput extends StandardJSONSchemaV1, TOutput extends StandardJSONSchemaV1 | undefined = undefined> {
   description?: string
-  execute: (input: StandardSchemaV1.InferInput<TInput>) => TOutput extends StandardJSONSchemaV1
+  execute?: (input: StandardSchemaV1.InferInput<TInput>) => TOutput extends StandardJSONSchemaV1
     ? Promise<StandardSchemaV1.InferInput<TOutput>> | StandardSchemaV1.InferInput<TOutput>
     : Promise<string | ToolResultPartContent[]> | string | ToolResultPartContent[]
   inputSchema: TInput
@@ -21,20 +24,37 @@ export interface ToolOptions<TInput extends StandardJSONSchemaV1, TOutput extend
   outputSchema?: TOutput
 }
 
-export const tool = <TInput extends StandardJSONSchemaV1, TOutput extends StandardJSONSchemaV1 | undefined = undefined>(options: ToolOptions<TInput, TOutput>): Tool => ({
-  description: options.description,
-  execute: options.execute != null
-    ? async (input) => {
-      // TODO: validate
-      // eslint-disable-next-line ts/await-thenable
-      const result = await options.execute(input)
-      if (options.outputSchema) // TODO: validate
-        return JSON.stringify(result)
-      else
-        return result as string | ToolResultPartContent[]
+interface ToolFactory {
+  <TInput extends StandardJSONSchemaV1, TOutput extends StandardJSONSchemaV1 | undefined = undefined>(
+    options: Required<Pick<ToolOptions<TInput, TOutput>, 'execute'>> & ToolOptions<TInput, TOutput>,
+  ): ExecutableTool
+  <TInput extends StandardJSONSchemaV1, TOutput extends StandardJSONSchemaV1 | undefined = undefined>(
+    options: ToolOptions<TInput, TOutput>,
+  ): Tool
+}
+
+export const tool = ((options: ToolOptions<StandardJSONSchemaV1, StandardJSONSchemaV1 | undefined>): ExecutableTool | Tool => {
+  const tool: Tool = {
+    description: options.description,
+    inputSchema: options.inputSchema['~standard'].jsonSchema.input({ target: 'draft-07' }),
+    name: options.name,
+    outputSchema: options.outputSchema?.['~standard'].jsonSchema.input({ target: 'draft-07' }),
+  }
+
+  if (options.execute == null) {
+    return tool
+  }
+  else {
+    return {
+      ...tool,
+      execute: async (input: unknown) => {
+        // TODO: validate
+        const result = await options.execute!(input)
+        if (options.outputSchema) // TODO: validate
+          return JSON.stringify(result)
+        else
+          return result as string | ToolResultPartContent[]
+      },
     }
-    : undefined,
-  inputSchema: options.inputSchema['~standard'].jsonSchema.input({ target: 'draft-07' }),
-  name: options.name,
-  outputSchema: options.outputSchema?.['~standard'].jsonSchema.input({ target: 'draft-07' }),
-})
+  }
+}) as ToolFactory
